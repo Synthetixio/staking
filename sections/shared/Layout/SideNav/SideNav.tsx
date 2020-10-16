7b4b3028512aa4b30ad9fd44c0824e79958f67e9
AndyTCF <andy.c.f.98@gmail.com>
2020-10-16 15:02:39 +1100

    Add pricing in side nav

--- sections/shared/Layout/SideNav/SideNav.tsx
+++ sections/shared/Layout/SideNav/SideNav.tsx
@@ -5,8 +5,10 @@ import Link from 'next/link';
 
 import { useTranslation } from 'react-i18next';
 
-import { linkCSS } from 'styles/common';
+import { FlexDiv, FlexDivCentered, FlexDivRowCentered, linkCSS } from 'styles/common';
 import StakingLogo from 'assets/inline-svg/app/staking-logo.svg';
+import ChangePositive from 'assets/inline-svg/app/change-positive.svg';
+import ChangeNegative from 'assets/inline-svg/app/change-negative.svg';
 
 import { MENU_LINKS } from '../constants';
 
@@ -16,11 +18,11 @@ const SideNav: FC = () => {
 
 	return (
 		<SideNavContainer>
-			<StatsLogoWrap>
+			<StakingLogoWrap>
 				<Link href="/">
 					<StakingLogo />
 				</Link>
-			</StatsLogoWrap>
+			</StakingLogoWrap>
 			<MenuLinks>
 				{MENU_LINKS.map(({ i18nLabel, link }) => (
 					<MenuLinkItem key={link} isActive={asPath.includes(link)}>
@@ -30,6 +32,28 @@ const SideNav: FC = () => {
 					</MenuLinkItem>
 				))}
 			</MenuLinks>
+			<PriceSection>
+				<PriceItem>
+					<h6>SNX Price</h6>
+					<PriceRow>
+						<p>$5.91</p>
+						<ChangeRow isPositive={true}>
+							<ChangePositive />
+							<p>5.45%</p>
+						</ChangeRow>
+					</PriceRow>
+				</PriceItem>
+				<PriceItem>
+					<h6>ETH Price</h6>
+					<PriceRow>
+						<p>$5.91</p>
+						<ChangeRow isPositive={false}>
+							<ChangeNegative />
+							<p>3.45%</p>
+						</ChangeRow>
+					</PriceRow>
+				</PriceItem>
+			</PriceSection>
 		</SideNavContainer>
 	);
 };
@@ -42,6 +66,7 @@ const SideNavContainer = styled.div`
 	padding-left: 40px;
 	top: 0;
 	left: 0;
+	background-color: ${(props) => props.theme.colors.mediumBlue};
 `;
 
 const MenuLinks = styled.div`
@@ -67,9 +92,42 @@ const MenuLinkItem = styled.div<{ isActive: boolean }>`
 	}
 `;
 
-const StatsLogoWrap = styled.div`
+const StakingLogoWrap = styled.div`
 	margin-bottom: 100px;
 	cursor: pointer;
+`;
+
+const PriceSection = styled.div`
+	position: absolute;
+	bottom: 0;
+	padding: 20px 0px;
+`;
+
+const PriceItem = styled.div`
+	margin: 10px 0px;
+
+	h6 {
+		font-family: ${(props) => props.theme.fonts.condensedMedium};
+		font-size: 16px;
+		font-weight: 400;
+		color: ${(props) => props.theme.colors.silver};
+		margin: 0;
+	}
+	p {
+		font-family: ${(props) => props.theme.fonts.mono};
+		text-transform: uppercase;
+		font-weight: 700;
+		font-size: 16px;
+		margin-right: 5px;
+	}
+`;
+
+const PriceRow = styled(FlexDivCentered)``;
+
+const ChangeRow = styled(FlexDivCentered)<{ isPositive: boolean }>`
+	p {
+		margin-left: 5px;
+	}
 `;
 
 export default SideNav;
