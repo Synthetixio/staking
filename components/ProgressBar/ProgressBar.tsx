94ffae3e960c3a11f8d4b9eeb1cc1e33eea438b6
AndyTCF <andy.c.f.98@gmail.com>
2020-10-20 15:12:46 +1100

    work on progress bars

--- components/ProgressBar/ProgressBar.tsx
+++ components/ProgressBar/ProgressBar.tsx
@@ -1,24 +1,44 @@
 import { FC } from 'react';
 import styled from 'styled-components';
+import { FlexDivRowCentered } from 'styles/common';
 
 type ProgressBarProps = {
 	percentage: number;
+	borderColor: string;
+	fillColor: string;
+	glowColor?: string;
 };
 
-const ProgressBar: FC<ProgressBarProps> = ({ percentage }) => (
-	<ProgressBarWrapper>
-		<Bar percentage={percentage} />
+const ProgressBar: FC<ProgressBarProps> = ({
+	percentage,
+	borderColor,
+	fillColor,
+	glowColor = 'none',
+}) => (
+	<ProgressBarWrapper borderColor={borderColor}>
+		<Bar percentage={percentage} fillColor={fillColor} glowColor={glowColor} />
+		<UnfilledBar percentage={1 - percentage} borderColor={borderColor} />
 	</ProgressBarWrapper>
 );
 
-const ProgressBarWrapper = styled.div`
-	background-color: ${(props) => props.theme.colors.mediumBlue};
+const ProgressBarWrapper = styled(FlexDivRowCentered)<{ borderColor: string }>`
+	height: 8px;
 `;
 
-const Bar = styled.div<{ percentage: number }>`
+const Bar = styled.div<{ percentage: number; fillColor: string; glowColor: string }>`
 	height: 100%;
 	width: ${(props) => props.percentage * 100}%;
-	border: 2px solid ${(props) => props.theme.colors.goldColors.color1};
+	border: none;
+	background-color: ${(props) => props.fillColor};
+	border: 1px solid ${(props) => props.fillColor};
+	box-shadow: ${(props) => props.glowColor};
+`;
+
+const UnfilledBar = styled.div<{ percentage: number; borderColor: string }>`
+	height: 100%;
+	width: ${(props) => props.percentage * 100}%;
+	border: 1px solid ${(props) => props.borderColor};
+	border-left: none;
 `;
 
 export default ProgressBar;
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,11 +1,36 @@
 import Head from 'next/head';
-import styled from 'styled-components';
+import styled, { useTheme } from 'styled-components';
 import { useTranslation } from 'react-i18next';
-import { FlexDiv, FlexDivCol, FlexDivColCentered, FlexDivRowCentered } from 'styles/common';
+import {
+	FlexDiv,
+	FlexDivCentered,
+	FlexDivCol,
+	FlexDivColCentered,
+	FlexDivRowCentered,
+} from 'styles/common';
 import SNXStatBackground from 'assets/svg/snx-stat-background.svg';
+import ProgressBar from 'components/ProgressBar';
 
 const DashboardPage = () => {
 	const { t } = useTranslation();
+	const theme = useTheme();
+
+	// const totalValueLocked = 	SNXPercentLocked * SNXTotalSupply * (SNXPrice ?? 0)
+
+	// 	const currentSnxStakingApy = (((sUSDPrice ?? 0) * currentFeePeriod.feesToDistribute +
+	// 	(SNXPrice ?? 0) * currentFeePeriod.rewardsToDistribute) *
+	// 	52) /
+	// SNXValueStaked;
+
+	// const activeDebt =
+
+	const returnActionBox = () => (
+		<ActionBox>
+			<ActionIcon>{icon}</ActionIcon>
+			<ActionCopy>{icon}</ActionCopy>
+			<ActionButton href={link}>{copy}</ActionButton>
+		</ActionBox>
+	);
 
 	return (
 		<>
@@ -16,42 +41,74 @@ const DashboardPage = () => {
 				<StatsSection>
 					{/* @TODO Refactor to StatBox.tsx */}
 					<StatBox
+						key={'staked-value'}
 						style={{
 							backgroundImage: `url(${SNXStatBackground})`,
 						}}
 					>
-						<StatTitle titleColor={'brightBlue'}>{t('dashboard.stat-box.staked-value')}</StatTitle>
-						<StatValue>$104, 128.31</StatValue>
+						<StatTitle titleColor={theme.colors.brightBlue}>
+							{t('dashboard.stat-box.staked-value')}
+						</StatTitle>
+						<StatValue>
+							{/* {SNXPercentLocked * SNXTotalSupply * (SNXPrice ?? 0)} */}
+							$134,234
+						</StatValue>
 					</StatBox>
 
 					<StatBox
+						key={'earning'}
 						style={{
 							backgroundImage: `url(${SNXStatBackground})`,
 						}}
 					>
-						<StatTitle titleColor={'brightGreen'}>{t('dashboard.stat-box.earning')}</StatTitle>
+						<StatTitle titleColor={theme.colors.brightGreen}>
+							{t('dashboard.stat-box.earning')}
+						</StatTitle>
 						<StatValue>6.14%</StatValue>
 					</StatBox>
 
 					<StatBox
+						key={'active-debt'}
 						style={{
 							backgroundImage: `url(${SNXStatBackground})`,
 						}}
 					>
-						<StatTitle titleColor={'brightPink'}>{t('dashboard.stat-box.active-debt')}</StatTitle>
+						<StatTitle titleColor={theme.colors.brightPink}>
+							{t('dashboard.stat-box.active-debt')}
+						</StatTitle>
 						<StatValue>$13,461.23</StatValue>
 					</StatBox>
-
-					<BarStats>
-						<BarStatBox>
-							<BarHeaderSection>
-								<BarTitle></BarTitle>
-								{/* <BarPercentage></BarPercentage> */}
-							</BarHeaderSection>
-							<BarContent></BarContent>
-						</BarStatBox>
-					</BarStats>
 				</StatsSection>
+
+				<BarStats>
+					<BarStatBox key="CRATIO">
+						<BarHeaderSection>
+							<BarTitle>{t('dashboard.bar.c-ratio')}</BarTitle>
+							<BarValue>622%</BarValue>
+						</BarHeaderSection>
+						<ProgressBar
+							percentage={0.5}
+							borderColor={theme.colors.brightPink}
+							fillColor={theme.colors.brightBlue}
+							glowColor={`0px 0px 15px rgba(77, 244, 184, 0.25);`}
+						/>
+					</BarStatBox>
+					<BarStatBox key="PERIOD">
+						<BarHeaderSection>
+							<BarTitle>{t('dashboard.bar.period')}</BarTitle>
+							<BarValue>56:35:10</BarValue>
+						</BarHeaderSection>
+						<ProgressBar
+							percentage={0.8}
+							borderColor={theme.colors.brightGreen}
+							fillColor={theme.colors.brightGreen}
+							glowColor={`0px 0px 15px rgba(77, 244, 184, 0.25);`}
+						/>
+					</BarStatBox>
+				</BarStats>
+				<Actions>
+					<ActionTitle>{t('dashboard.actions.title')}</ActionTitle>
+				</Actions>
 			</Content>
 		</>
 	);
@@ -59,7 +116,6 @@ const DashboardPage = () => {
 
 const Content = styled(FlexDivCol)`
 	justify-content: center;
-	align-items: center;
 `;
 
 const StatsSection = styled(FlexDivRowCentered)``;
@@ -76,7 +132,7 @@ const StatBox = styled(FlexDivColCentered)`
 const StatTitle = styled.p<{ titleColor: string }>`
 	font-family: ${(props) => props.theme.fonts.mono};
 	font-size: 14px;
-	color: ${(props: any) => props.theme.colors[props.titleColor]};
+	color: ${(props) => props.titleColor};
 	margin: 0;
 `;
 
@@ -88,10 +144,64 @@ const StatValue = styled.p`
 
 const BarStats = styled(FlexDiv)``;
 
-const BarStatBox = styled(FlexDivCol)``;
+const BarStatBox = styled(FlexDivCol)`
+	margin-right: 16px;
+	width: 400px;
+`;
 
-const BarHeaderSection = styled(FlexDivRowCentered)``;
-const BarTitle = styled.p``;
-const BarContent = styled.div``;
+const BarHeaderSection = styled(FlexDivRowCentered)`
+	justify-content: space-between;
+`;
+const BarTitle = styled.p`
+	font-size: 14px;
+	font-family: ${(props) => props.theme.fonts.condensedMedium};
+	color: ${(props) => props.theme.colors.silver};
+`;
+const BarValue = styled.p`
+	font-size: 16px;
+	color: ${(props) => props.theme.colors.white};
+	font-family: ${(props) => props.theme.fonts.mono};
+`;
+
+const Actions = styled(FlexDivCol)``;
+
+const ActionTitle = styled.p`
+	font-size: 14px;
+	font-family: ${(props) => props.theme.fonts.condensedMedium};
+	color: ${(props) => props.theme.colors.silver};
+	text-align: left;
+`;
+
+const ActionBox = styled(FlexDivCentered)``;
+
+const ActionIcon = styled.div``;
+
+const ActionCopy = styled.p``;
+
+const ActionButton = styled.div`
+	width: 161px;
+	height: 48px;
+
+	background: #0c2344;
+	border: 1px solid #00d1ff;
+	box-sizing: border-box;
+	box-shadow: 0px 0px 10px rgba(0, 209, 255, 0.9);
+	border-radius: 4px;
+
+	span {
+		font-family: GT America;
+		font-style: normal;
+		font-weight: bold;
+		font-size: 12px;
+		line-height: 24px;
+
+		display: flex;
+		align-items: center;
+		text-align: center;
+		text-transform: uppercase;
+
+		color: #00d1ff;
+	}
+`;
 
 export default DashboardPage;
