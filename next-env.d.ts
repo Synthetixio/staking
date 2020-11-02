2b6d93e1e57a518f60c35b9d01fddb2c0a68b8ed
AndyTCF <andy.c.f.98@gmail.com>
2020-11-02 12:04:06 +1100

    configure next-env

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-/// <reference types="next-images" />
 /// <reference types="optimized-images-loader" />
 
 declare global {
