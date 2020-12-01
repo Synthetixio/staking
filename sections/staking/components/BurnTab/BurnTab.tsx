59f55bf313a5914a32d0591bce538ffd58ce043c
AndyTCF <andy.c.f.98@gmail.com>
2020-12-01 20:31:59 +1100

    implement burn tiles

--- sections/staking/components/BurnTab/BurnTab.tsx
+++ sections/staking/components/BurnTab/BurnTab.tsx
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { LoadingState } from 'constants/loading';
 import styled from 'styled-components';
+import Img, { Svg } from 'react-optimized-image';
+import sUSDIcon from '@synthetixio/assets/synths/sUSD.svg';
+
 import {
 	TabContainer,
-	StyledButton,
 	StyledCTA,
 	StyledInput,
 	DataContainer,
@@ -12,6 +14,8 @@ import {
 	InputBox,
 	RowTitle,
 	RowValue,
+	InputContainer,
+	InputLocked,
 } from '../common';
 import { ModalContent, ModalItem, ModalItemTitle, ModalItemText } from 'styles/common';
 import { CRYPTO_CURRENCY_MAP, SYNTHS_MAP } from 'constants/currency';
@@ -24,11 +28,12 @@ import { useRecoilValue } from 'recoil';
 import { walletAddressState } from 'store/wallet';
 import synthetix from 'lib/synthetix';
 import GasSelector from 'components/GasSelector';
-import { FlexDivRowCentered } from 'styles/common';
 import { getMintAmount, getStakingAmount } from '../helper';
 import TxConfirmationModal from 'sections/shared/modals/TxConfirmationModal';
 import { formatCurrency } from 'utils/formatters/number';
-import Staking from 'sections/staking/context/StakingContext';
+import Staking, { BurnActionType } from 'sections/staking/context/StakingContext';
+import BurnTiles from '../BurnTiles';
+import NavigationBack from 'assets/svg/app/navigation-back.svg';
 
 type BurnTabProps = {
 	maxBurnAmount: number;
@@ -46,14 +51,15 @@ const BurnTab: React.FC<BurnTabProps> = ({
 	stakedSNX,
 }) => {
 	const { t } = useTranslation();
+	const { monitorHash } = Notify.useContainer();
+	const { amountToBurn, onBurnChange, burnType, onBurnTypeChange } = Staking.useContainer();
+	const walletAddress = useRecoilValue(walletAddressState);
+
 	const [burningTxError, setBurningTxError] = useState<boolean>(false);
 	const [error, setError] = useState<string | null>(null);
 	const [gasLimitEstimate, setGasLimitEstimate] = useState<number | null>(null);
 	const [burnLoadingState] = useState<LoadingState | null>(null);
-	const { monitorHash } = Notify.useContainer();
-	const { amountToBurn, onBurnChange } = Staking.useContainer();
 	const [txModalOpen, setTxModalOpen] = useState<boolean>(false);
-	const walletAddress = useRecoilValue(walletAddressState);
 	const [burnToTarget, setBurnToTarget] = useState<boolean>(false);
 	const [stakingCurrencyKey] = useState<string>(CRYPTO_CURRENCY_MAP.SNX);
 	const [synthCurrencyKey] = useState<string>(SYNTHS_MAP.sUSD);
@@ -139,115 +145,120 @@ const BurnTab: React.FC<BurnTabProps> = ({
 		}
 	};
 
-	return (
-		<>
-			<TabContainer>
-				<InputBox>
-					<StyledInput
-						placeholder="0"
-						onChange={(e) => handleBurnChange(e.target.value)}
-						value={amountToBurn}
+	/**
+	 * Given the amount to mint, returns the equivalent collateral needed for stake.
+	 * @param mintInput Amount to mint
+	 */
+	const stakeInfo = (mintInput: string) =>
+		formatCurrency(stakingCurrencyKey, getStakingAmount(targetCRatio, mintInput, SNXRate), {
+			currencyKey: stakingCurrencyKey,
+		});
+
+	/**
+	 * Given the amount to stake, returns the equivalent debt produced. (Estimate)
+	 * @param mintInput Amount to mint
+	 */
+	const mintInfo = (stakeInput: string) =>
+		formatCurrency(synthCurrencyKey, getMintAmount(targetCRatio, stakeInput, SNXRate), {
+			currencyKey: synthCurrencyKey,
+		});
+
+	const formattedDebt = formatCurrency(synthCurrencyKey, amountToBurn, {
+		currencyKey: synthCurrencyKey,
+	});
+
+	const formattedStake = formatCurrency(stakingCurrencyKey, maxCollateral.toString(), {
+		currencyKey: stakingCurrencyKey,
+	});
+
+	const returnInput = (inputTypes: { max?: boolean; custom?: boolean; target?: boolean }) => {
+		const { max, custom, target } = inputTypes;
+		return (
+			<>
+				<InputContainer>
+					<IconContainer onClick={() => onBurnTypeChange(null)}>
+						<Svg src={NavigationBack} />
+					</IconContainer>
+					<InputBox>
+						<Img width={50} height={50} src={sUSDIcon} />
+						{max || target ? (
+							<InputLocked>{mintInfo(maxBurnAmount.toString())}</InputLocked>
+						) : (
+							<StyledInput placeholder="0" onChange={(e) => onBurnChange(e.target.value)} />
+						)}
+					</InputBox>
+					<DataContainer>
+						<DataRow>
+							<RowTitle>{t('staking.actions.burn.info.unstaking')}</RowTitle>
+							<RowValue>{max ? stakeInfo(amountToBurn) : 0}</RowValue>
+						</DataRow>
+						<DataRow>
+							<GasSelector gasLimitEstimate={gasLimitEstimate} setGasPrice={setGasPrice} />
+						</DataRow>
+					</DataContainer>
+					{amountToBurn !== '0' && amountToBurn !== '' ? (
+						<StyledCTA
+							blue={false}
+							onClick={handleBurn}
+							variant="alt"
+							size="lg"
+							disabled={!!burnLoadingState}
+						>
+							{t('staking.actions.burn.action.burn', {
+								amountToBurn: amountToBurn,
+								stakeType: stakingCurrencyKey,
+							})}
+						</StyledCTA>
+					) : (
+						<StyledCTA blue={false} variant="alt" size="lg" disabled={true}>
+							{t('staking.actions.mint.action.empty')}
+						</StyledCTA>
+					)}
+				</InputContainer>
+				{txModalOpen && (
+					<TxConfirmationModal
+						onDismiss={() => setTxModalOpen(false)}
+						txError={burningTxError}
+						attemptRetry={handleBurn}
+						content={
+							<ModalContent>
+								<ModalItem>
+									<ModalItemTitle>{t('modals.confirm-transaction.staking.from')}</ModalItemTitle>
+									<ModalItemText>{formattedDebt}</ModalItemText>
+								</ModalItem>
+								<ModalItem>
+									<ModalItemTitle>{t('modals.confirm-transaction.staking.to')}</ModalItemTitle>
+									<ModalItemText>{stakeInfo(amountToBurn)}</ModalItemText>
+								</ModalItem>
+							</ModalContent>
+						}
 					/>
-				</InputBox>
-				<FlexDivRowCentered>
-					<StyledButton
-						blue={false}
-						style={{ width: '50%', marginRight: 5 }}
-						onClick={handleMaxBurn}
-						variant="outline"
-					>
-						{t('staking.actions.burn.action.max')}
-					</StyledButton>
-					<StyledButton
-						blue={false}
-						style={{ width: '50%', marginLeft: 5 }}
-						onClick={handleBurnToTarget}
-						variant="outline"
-					>
-						{t('staking.actions.burn.action.to-target')}
-					</StyledButton>
-				</FlexDivRowCentered>
-				<DataContainer>
-					<DataRow>
-						<RowTitle>{t('staking.actions.burn.info.burning')}</RowTitle>
-						<RowValue>
-							{amountToBurn} {synthCurrencyKey}
-						</RowValue>
-					</DataRow>
-					<DataRow>
-						<RowTitle>{t('staking.actions.burn.info.unstaking')}</RowTitle>
-						<RowValue>
-							{maxBurnAmount === Number(amountToBurn)
-								? stakedSNX
-								: getStakingAmount(targetCRatio, amountToBurn.toString(), SNXRate)}
-							{stakingCurrencyKey}
-						</RowValue>
-					</DataRow>
-				</DataContainer>
-				<StyledGasContainer gasLimitEstimate={gasLimitEstimate} setGasPrice={setGasPrice} />
-				{amountToBurn !== '0' && amountToBurn !== '' ? (
-					<StyledCTA
-						blue={false}
-						onClick={handleBurn}
-						variant="alt"
-						size="lg"
-						disabled={!!burnLoadingState}
-					>
-						{t('staking.actions.burn.action.burn', {
-							amountToBurn: amountToBurn,
-							stakeType: stakingCurrencyKey,
-						})}
-					</StyledCTA>
-				) : (
-					<StyledCTA blue={false} variant="alt" size="lg" disabled={true}>
-						{t('staking.actions.mint.action.empty')}
-					</StyledCTA>
 				)}
-			</TabContainer>
-			{txModalOpen && (
-				<TxConfirmationModal
-					onDismiss={() => setTxModalOpen(false)}
-					txError={burningTxError}
-					attemptRetry={handleBurn}
-					content={
-						<ModalContent>
-							<ModalItem>
-								<ModalItemTitle>{t('modals.confirm-transaction.staking.from')}</ModalItemTitle>
-								<ModalItemText>
-									{formatCurrency(stakingCurrencyKey, amountToBurn, {
-										currencyKey: stakingCurrencyKey,
-										decimals: 4,
-									})}
-								</ModalItemText>
-							</ModalItem>
-							<ModalItem>
-								<ModalItemTitle>{t('modals.confirm-transaction.staking.to')}</ModalItemTitle>
-								<ModalItemText>
-									{formatCurrency(
-										synthCurrencyKey,
-										getMintAmount(
-											targetCRatio,
-											getStakingAmount(targetCRatio, amountToBurn.toString(), SNXRate).toString(),
-											SNXRate
-										).toString(),
-										{
-											currencyKey: synthCurrencyKey,
-											decimals: 4,
-										}
-									)}
-								</ModalItemText>
-							</ModalItem>
-						</ModalContent>
-					}
-				/>
-			)}
-		</>
-	);
+			</>
+		);
+	};
+
+	const returnPanel = useMemo(() => {
+		switch (burnType) {
+			case BurnActionType.MAX:
+				return returnInput({ max: true });
+			case BurnActionType.CUSTOM:
+				return returnInput({ custom: true });
+			case BurnActionType.TARGET:
+				return returnInput({ target: true });
+			default:
+				return <BurnTiles targetCRatio={targetCRatio} />;
+		}
+	}, [burnType, amountToBurn, targetCRatio]);
+
+	return <TabContainer>{returnPanel}</TabContainer>;
 };
 
-const StyledGasContainer = styled(GasSelector)`
-	margin: 16px 0px;
-	flex-direction: row;
+const IconContainer = styled.div`
+	position: absolute;
+	top: 20px;
+	left: 20px;
+	cursor: pointer;
 `;
 
 export default BurnTab;
