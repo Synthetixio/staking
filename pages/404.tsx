906b9313c5409539415d3c6c3187f35d7f112f00
Evgeny Boxer <evgeny.boxer@gmail.com>
2020-12-09 21:15:54 +1100

    feat: match 404 page to design

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,9 @@
 import Head from 'next/head';
 import { useTranslation } from 'react-i18next';
 import styled from 'styled-components';
-import { Svg } from 'react-optimized-image';
 
 import media from 'styles/media';
-import { FlexDivRowCentered } from 'styles/common';
-
-import CaretLeftXLIcon from 'assets/svg/app/caret-left-xl.svg';
-import CaretRightXLICon from 'assets/svg/app/caret-right-xl.svg';
+import { absoluteCenteredCSS, FlexDivRowCentered } from 'styles/common';
 
 const NotFoundPage = () => {
 	const { t } = useTranslation();
@@ -18,60 +14,39 @@ const NotFoundPage = () => {
 				<title>{t('not-found.page-title')}</title>
 			</Head>
 			<Container>
-				<Svg src={CaretLeftXLIcon} />
-				<Content>
-					<Title>{t('not-found.title')}</Title>
-					<Subtitle>{t('not-found.subtitle')}</Subtitle>
-				</Content>
-				<Svg src={CaretRightXLICon} />
+				<Title>{t('not-found.title')}</Title>
+				<Subtitle>{t('not-found.subtitle')}</Subtitle>
 			</Container>
 		</>
 	);
 };
 
-const Container = styled(FlexDivRowCentered)`
-	flex-grow: 1;
-	justify-content: center;
-	display: flex;
-	align-items: center;
-	> * {
-		flex-shrink: 0;
-	}
-	padding: 0 20px;
-	${media.lessThan('sm')`
-		flex-direction: column;
-		svg {
-			transform: rotate(90deg);
-		}
-	`}
+const Container = styled.div`
+	${absoluteCenteredCSS};
+	text-align: center;
+	margin-top: -32px;
 `;
 
-const Content = styled.div`
-	color: ${(props) => props.theme.colors.white};
-	font-family: ${(props) => props.theme.fonts.mono};
-	padding: 0 48px;
-	text-align: center;
-	margin-top: -10px;
+const headingMixin = `
+	margin: 0;
+	font-weight: normal;
+	line-height: normal;
 `;
 
 const Title = styled.h1`
-	font-size: 120px;
-	margin: 0;
-	font-weight: normal;
-	line-height: normal;
-	${media.lessThan('sm')`
-		font-size: 80px;
-	`}
+	${headingMixin};
+	color: ${(props) => props.theme.colors.black};
+	font-family: ${(props) => props.theme.fonts.mono};
+	-webkit-text-stroke: 1px ${(props) => props.theme.colors.pink};
+	font-size: 160px;
 `;
 
 const Subtitle = styled.h2`
-	margin: 0;
-	font-size: 20px;
-	line-height: normal;
-	font-weight: normal;
-	${media.lessThan('sm')`
-		font-size: 14px;
-	`}
+	${headingMixin};
+	text-transform: uppercase;
+	color: ${(props) => props.theme.colors.white};
+	font-family: ${(props) => props.theme.fonts.expanded};
+	font-size: 24px;
 `;
 
 export default NotFoundPage;
