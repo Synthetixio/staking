bc34e770e5b7bf2601a98ff416e162818d14d4ba
dvd-schwrtz <dbschwartz17@gmail.com>
2020-12-10 09:54:23 -0800

    Add transaction workflows

--- sections/earn/ClaimTab/ClaimTab.tsx
+++ sections/earn/ClaimTab/ClaimTab.tsx
@@ -2,17 +2,29 @@ import React, { useEffect, useState } from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import styled from 'styled-components';
 import { Svg } from 'react-optimized-image';
+import { ethers } from 'ethers';
 
-import { ErrorMessage, FlexDivCentered, FlexDivColCentered } from 'styles/common';
+import {
+	ErrorMessage,
+	FlexDivCentered,
+	FlexDivColCentered,
+	ModalContent,
+	ModalItem,
+	ModalItemTitle,
+	ModalItemText,
+} from 'styles/common';
 import useClaimedStatus from 'sections/hooks/useClaimedStatus';
 import BigNumber from 'bignumber.js';
 import { formatCurrency, formatFiatCurrency } from 'utils/formatters/number';
 import synthetix from 'lib/synthetix';
 import Connector from 'containers/Connector';
 import { getGasEstimateForTransaction } from 'utils/transactions';
+import { Transaction } from 'constants/network';
 import Etherscan from 'containers/Etherscan';
 import GasSelector from 'components/GasSelector';
+import Notify from 'containers/Notify';
 import { normalizedGasPrice, normalizeGasLimit } from 'utils/network';
+import TxConfirmationModal from 'sections/shared/modals/TxConfirmationModal/TxConfirmationModal';
 import { CRYPTO_CURRENCY_MAP, SYNTHS_MAP } from 'constants/currency';
 import snxSVG from 'assets/svg/incentives/pool-snx.svg';
 import largeWaveSVG from 'assets/svg/app/large-wave.svg';
@@ -41,107 +53,129 @@ const ClaimTab: React.FC<ClaimTabProps> = ({
 }) => {
 	const { t } = useTranslation();
 	const claimed = useClaimedStatus();
+	const { monitorHash } = Notify.useContainer();
 	const { etherscanInstance } = Etherscan.useContainer();
 	const { notify } = Connector.useContainer();
 	const [gasLimitEstimate, setGasLimitEstimate] = useState<number | null>(null);
 	const [gasPrice, setGasPrice] = useState<number>(0);
 	const [error, setError] = useState<string | null>(null);
 
+	const [transactionState, setTransactionState] = useState<Transaction>(Transaction.PRESUBMIT);
+	const [txHash, setTxHash] = useState<string | null>(null);
+	const [txModalOpen, setTxModalOpen] = useState<boolean>(false);
+
 	useEffect(() => {
 		const getGasLimitEstimate = async () => {
 			if (synthetix && synthetix.js) {
 				try {
-					const gasEstimate = await getGasEstimateForTransaction(
-						[],
-						synthetix.js?.contracts.FeePool.estimateGas.claimFees
-					);
+					setError(null);
+					const {
+						contracts: { FeePool },
+					} = synthetix.js as SynthetixJS;
+					let gasEstimate = await getGasEstimateForTransaction([], FeePool.estimateGas.claimFees);
 					setGasLimitEstimate(normalizeGasLimit(Number(gasEstimate)));
 				} catch (error) {
 					if (!error.message.includes('already claimed')) {
 						setError(error.message);
-						setGasLimitEstimate(null);
 					}
+					setGasLimitEstimate(null);
 				}
 			}
 		};
 		getGasLimitEstimate();
-		// eslint-disable-next-line
-	}, [synthetix, error]);
+	}, [synthetix]);
 
 	const handleClaim = async () => {
 		try {
+			setError(null);
+			setTxModalOpen(true);
 			const {
 				contracts: { FeePool },
 			} = synthetix.js!;
-			const tx = await FeePool.claimFees({
-				gasPrice: normalizedGasPrice(gasPrice),
-				gasLimitEstimate,
-			});
-			if (notify) {
-				const { emitter } = notify.hash(tx.hash);
-				const link = etherscanInstance != null ? etherscanInstance.txLink(tx.hash) : undefined;
-				emitter.on('txConfirmed', () => {
-					refetch();
-					return {
-						autoDismiss: 0,
-						link,
-					};
-				});
 
-				emitter.on('all', () => {
-					return {
-						link,
-					};
+			let transaction: ethers.ContractTransaction;
+			const gasLimit = getGasEstimateForTransaction([], FeePool.estimateGas.claimFees);
+			transaction = await FeePool.claimFees({
+				gasPrice: normalizedGasPrice(gasPrice),
+				gasLimit,
+			});
+
+			if (transaction) {
+				setTxHash(transaction.hash);
+				setTransactionState(Transaction.WAITING);
+				monitorHash({
+					txHash: transaction.hash,
+					onTxConfirmed: () => setTransactionState(Transaction.SUCCESS),
 				});
+				setTxModalOpen(false);
 			}
 		} catch (e) {
-			console.log(e);
+			setTransactionState(Transaction.PRESUBMIT);
+			setError(e.message);
 		}
 	};
 
 	return (
-		<TabContainer>
-			<Label>
-				<Trans i18nKey="earn.incentives.options.snx.description" components={[<StyledLink />]} />
-			</Label>
+		<>
+			<TabContainer>
+				<Label>
+					<Trans i18nKey="earn.incentives.options.snx.description" components={[<StyledLink />]} />
+				</Label>
 
-			<InnerContainer>
-				<ValueBoxWrapper>
-					<ValueBox>
-						<Svg src={snxSVG} />
+				<InnerContainer>
+					<ValueBoxWrapper>
+						<ValueBox>
+							<Svg src={snxSVG} />
+							<Value>
+								{formatCurrency(SYNTHS_MAP.sUSD, tradingRewards, {
+									currencyKey: SYNTHS_MAP.sUSD,
+									decimals: 2,
+								})}
+							</Value>
+							<Subtext>{t('earn.incentives.options.snx.trading-rewards')}</Subtext>
+						</ValueBox>
+						<ValueBox>
+							<Svg src={snxSVG} />
+							<Value>
+								{formatCurrency(CRYPTO_CURRENCY_MAP.SNX, stakingRewards, {
+									currencyKey: CRYPTO_CURRENCY_MAP.SNX,
+								})}
+							</Value>
+							<Subtext>{t('earn.incentives.options.snx.staking-rewards')}</Subtext>
+						</ValueBox>
+					</ValueBoxWrapper>
+					<TotalValueWrapper>
+						<Subtext>{t('earn.incentives.options.snx.total-value')}</Subtext>
 						<Value>
-							{formatCurrency(SYNTHS_MAP.sUSD, tradingRewards, {
-								currencyKey: SYNTHS_MAP.sUSD,
-								decimals: 2,
+							{formatFiatCurrency(totalRewards, {
+								sign: '$',
 							})}
 						</Value>
-						<Subtext>{t('earn.incentives.options.snx.trading-rewards')}</Subtext>
-					</ValueBox>
-					<ValueBox>
-						<Svg src={snxSVG} />
-						<Value>
-							{formatCurrency(CRYPTO_CURRENCY_MAP.SNX, stakingRewards, {
-								currencyKey: CRYPTO_CURRENCY_MAP.SNX,
-							})}
-						</Value>
-						<Subtext>{t('earn.incentives.options.snx.staking-rewards')}</Subtext>
-					</ValueBox>
-				</ValueBoxWrapper>
-				<TotalValueWrapper>
-					<Subtext>{t('earn.incentives.options.snx.total-value')}</Subtext>
-					<Value>
-						{formatFiatCurrency(totalRewards, {
-							sign: '$',
-						})}
-					</Value>
-				</TotalValueWrapper>
-				{error && <ErrorMessage>{error}</ErrorMessage>}
-				<PaddedButton variant="primary" onClick={handleClaim} disabled={error != null || claimed}>
-					{claimed ? t('earn.actions.claim.claimed-button') : t('earn.actions.claim.claim-button')}
-				</PaddedButton>
-				<GasSelector gasLimitEstimate={gasLimitEstimate} setGasPrice={setGasPrice} />
-			</InnerContainer>
-		</TabContainer>
+					</TotalValueWrapper>
+					{error && <ErrorMessage>{error}</ErrorMessage>}
+					<PaddedButton variant="primary" onClick={handleClaim} disabled={error != null || claimed}>
+						{claimed
+							? t('earn.actions.claim.claimed-button')
+							: t('earn.actions.claim.claim-button')}
+					</PaddedButton>
+					<GasSelector gasLimitEstimate={gasLimitEstimate} setGasPrice={setGasPrice} />
+				</InnerContainer>
+			</TabContainer>
+			{txModalOpen && (
+				<TxConfirmationModal
+					onDismiss={() => setTxModalOpen(false)}
+					txError={error}
+					attemptRetry={handleClaim}
+					content={
+						<ModalContent>
+							<ModalItem>
+								<ModalItemTitle>{t('modals.confirm-transaction.claiming.claiming')}</ModalItemTitle>
+							</ModalItem>
+						</ModalContent>
+					}
+				/>
+			)}
+		</>
 	);
 };
 
