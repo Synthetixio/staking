f46650bc650ef9bcdb1359b89ffd75237b755fec
AndyTCF <andy.c.f.98@gmail.com>
2020-12-14 14:58:22 +1100

    add slick carousel

--- sections/dashboard/PossibleActions/Layouts/WizardGridBox.tsx
+++ sections/dashboard/PossibleActions/Layouts/WizardGridBox.tsx
@@ -1,6 +1,5 @@
 import { FC } from 'react';
-import StepWizard, { StepWizardChildProps } from 'react-step-wizard';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import Img from 'react-optimized-image';
 
 import Welcome from 'assets/png/wizard/welcome.png';
@@ -11,129 +10,105 @@ import Debt from 'assets/png/wizard/debt.png';
 
 import { GridBoxContainer } from './GridBox';
 import { useTranslation } from 'react-i18next';
-import { FlexDiv, FlexDivCentered } from 'styles/common';
+import { FlexDivCentered } from 'styles/common';
+import Slider from 'react-slick';
+import media from 'styled-media-query';
 
 type WizardGridBoxProps = {
 	gridLocations: [string, string, string, string];
 };
 
-export const WizardGridBox: FC<WizardGridBoxProps> = ({ gridLocations }) => (
-	<GridBoxContainer
-		columnStart={gridLocations[0]}
-		columnEnd={gridLocations[1]}
-		rowStart={gridLocations[2]}
-		rowEnd={gridLocations[3]}
-	>
-		<WizardWrap>
-			{/* @ts-ignore */}
-			<StepWizard nav={<Nav />} isLazyMount>
-				<WizardPage
-					icon={<Img src={Welcome} />}
-					title="homepage.welcome.title"
-					subtitle="homepage.welcome.subtitle"
-				/>
-				<WizardPage
-					icon={<Img src={What} />}
-					title="homepage.what.title"
-					subtitle="homepage.what.subtitle"
-				/>
-				<WizardPage
-					icon={<Img src={Why} />}
-					title="homepage.why.title"
-					subtitle="homepage.why.subtitle"
-				/>
-				<WizardPage
-					icon={<Img src={MintBurn} />}
-					title="homepage.mint-burn.title"
-					subtitle="homepage.mint-burn.subtitle"
-				/>
-				<WizardPage
-					icon={<Img src={Debt} />}
-					title="homepage.debt.title"
-					subtitle="homepage.debt.subtitle"
-				/>
-			</StepWizard>
-		</WizardWrap>
-	</GridBoxContainer>
-);
-
-const WizardWrap = styled.div`
-	width: 100%;
-	height: 100%;
-	position: relative;
-	overflow: hidden;
-	div {
-		z-index: 0;
-	}
-`;
-
-const Nav: FC<StepWizardChildProps> = (props) => {
-	const dots = [];
-	for (let i = 1; i <= props.totalSteps; i += 1) {
-		const isActive = props.currentStep === i;
-		dots.push(
-			<Dot key={`step-${i}`} isActive={isActive} onClick={() => props.goToStep(i)}>
-				&bull;
-			</Dot>
-		);
-	}
-
-	return <NavContainer>{dots}</NavContainer>;
-};
-
-const NavContainer = styled.div`
-	margin-bottom: 15px;
-	text-align: center;
-	display: flex;
-	justify-content: center;
-	position: absolute;
-	bottom: 0;
-	width: 100%;
-`;
-
-const Dot = styled.div<{ isActive: boolean }>`
-	color: black;
-	cursor: pointer;
-	font-size: 36px;
-	line-height: 1;
-	margin: 0 15px;
-	opacity: 0.4;
-	text-shadow: none;
-	transition: opacity 1s ease, text-shadow 1s ease;
-	will-change: opacity, text-shadow;
-	color: ${(props) => props.theme.colors.gray};
-	${(props) =>
-		props.isActive &&
-		css`
-			color: ${props.theme.colors.blue};
-			opacity: 1;
-			text-shadow: 0 0px 8px;
-		`}
-`;
-
-type WizardPageProps = {
-	title: string;
-	subtitle: string;
-	icon: React.ReactNode;
-};
-
-const WizardPage: React.FC<WizardPageProps> = ({ title, subtitle, icon }) => {
+export const WizardGridBox: FC<WizardGridBoxProps> = ({ gridLocations }) => {
 	const { t } = useTranslation();
+	const STEPS = [
+		{
+			icon: <Img src={Welcome} height={200} />,
+			title: 'homepage.welcome.title',
+			subtitle: 'homepage.welcome.subtitle',
+			id: 'welcome',
+		},
+		{
+			icon: <Img src={What} height={200} />,
+			title: 'homepage.what.title',
+			subtitle: 'homepage.what.subtitle',
+			id: 'what',
+		},
+		{
+			icon: <Img src={Why} height={200} />,
+			title: 'homepage.why.title',
+			subtitle: 'homepage.why.subtitle',
+			id: 'why',
+		},
+		{
+			icon: <Img src={MintBurn} height={200} />,
+			title: 'homepage.mint-burn.title',
+			subtitle: 'homepage.mint-burn.subtitle',
+			id: 'MintBurn',
+		},
+		{
+			icon: <Img src={Debt} height={200} />,
+			title: 'homepage.debt.title',
+			subtitle: 'homepage.debt.subtitle',
+			id: 'debt',
+		},
+	];
 	return (
-		<Page>
-			<IconContainer>{icon}</IconContainer>
-			<Title>{t(`${title}`)}</Title>
-			<Subtitle>{t(`${subtitle}`)}</Subtitle>
-		</Page>
+		<GridBoxContainer
+			columnStart={gridLocations[0]}
+			columnEnd={gridLocations[1]}
+			rowStart={gridLocations[2]}
+			rowEnd={gridLocations[3]}
+		>
+			<SliderContainer>
+				<Slider arrows={false} dots={true}>
+					{STEPS.map(({ id, icon, subtitle, title }) => (
+						<div key={id}>
+							<StepBox>
+								<IconContainer>{icon}</IconContainer>
+								<Title>{t(`${title}`)}</Title>
+								<Subtitle>{t(`${subtitle}`)}</Subtitle>
+							</StepBox>
+						</div>
+					))}
+				</Slider>
+			</SliderContainer>
+		</GridBoxContainer>
 	);
 };
-
-const Page = styled.div`
+const StepBox = styled.div`
 	text-align: center;
 	padding-top: 50px;
 	margin: 0px 24px;
 `;
-
+const SliderContainer = styled.div`
+	.slick-dots {
+		li {
+			margin: 0 2px;
+			button:before {
+				color: ${(props) => props.theme.colors.gray};
+				font-size: 9px;
+				opacity: 0.5;
+			}
+			&.slick-active {
+				button:before {
+					color: ${(props) => props.theme.colors.blue};
+					opacity: 1;
+				}
+			}
+		}
+		bottom: unset;
+		text-align: center;
+		${media.lessThan('medium')`
+			position: unset;
+			margin-top: 40px;
+			text-align: center;
+			top: unset;
+		`}
+	}
+	* {
+		outline: none;
+	}
+`;
 const IconContainer = styled(FlexDivCentered)`
 	height: 200px;
 	justify-content: center;
@@ -145,7 +120,6 @@ const Title = styled.p`
 	text-transform: uppercase;
 	padding-top: 16px;
 `;
-
 const Subtitle = styled.p`
 	font-family: ${(props) => props.theme.fonts.regular};
 	font-size: 12px;
