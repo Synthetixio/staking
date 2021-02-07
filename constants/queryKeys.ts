0604b3f154c8863524dbaf48436a414a155d48f3
mi-yu <m.yu8088@gmail.com>
2021-02-07 03:42:20 -0600

    Implement hedging warning
    Show warning when user's synth holdings out of proportion with overall
    pool's holdings.

--- constants/queryKeys.ts
+++ constants/queryKeys.ts
@@ -110,6 +110,7 @@ export const QUERY_KEYS = {
 			baseCurrencyKey,
 		],
 		TotalIssuedSynths: ['synths', 'totalIssuedSynths'],
+		TotalSupply: ['synths', 'totalSupply'],
 	},
 	Trades: {
 		AllTrades: ['trades', 'allTrades'],
