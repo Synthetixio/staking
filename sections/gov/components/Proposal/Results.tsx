2be6870fecf2a1ce418df9e5f69aa9989dbe2587
AndyTCF <andy.c.f.98@gmail.com>
2021-03-09 14:42:58 +1100

    map discord names to addresses

--- sections/gov/components/Proposal/Results.tsx
+++ sections/gov/components/Proposal/Results.tsx
@@ -9,7 +9,9 @@ import { formatNumber, formatPercent } from 'utils/formatters/number';
 import ProgressBar from 'components/ProgressBar';
 import { MaxHeightColumn, StyledTooltip } from 'sections/gov/components/common';
 import { SPACE_KEY } from 'constants/snapshot';
-import { getProfiles } from '../helper';
+import CouncilNominations from 'constants/nominations.json';
+import { useRecoilValue } from 'recoil';
+import { councilElectionCountState, numOfCouncilSeatsState } from 'store/gov';
 
 type ResultsProps = {
 	hash: string;
@@ -19,24 +21,26 @@ const Results: React.FC<ResultsProps> = ({ hash }) => {
 	const activeTab = useActiveTab();
 	const proposal = useProposal(activeTab, hash);
 	const [choices, setChoices] = useState<any>(null);
+	const electionCount = useRecoilValue(councilElectionCountState);
+	const numOfCouncilSeats = useRecoilValue(numOfCouncilSeatsState);
 
 	useEffect(() => {
-		if (proposal && proposal?.data?.choices && activeTab === SPACE_KEY.COUNCIL) {
-			const loadProfiles = async () => {
-				const profiles = await getProfiles(proposal?.data?.choices);
+		if (proposal && activeTab === SPACE_KEY.COUNCIL) {
+			const loadDiscordNames = async () => {
+				const currentElectionMembers = CouncilNominations as any;
 				const mappedProfiles = [] as any;
-				Object.keys(profiles).forEach((profile) => {
+
+				currentElectionMembers[electionCount].forEach((member: any) => {
 					mappedProfiles.push({
-						address: profile,
-						ens: profiles[profile].ens ? profiles[profile].ens : null,
-						name: profiles[profile].name ? profiles[profile].name : null,
+						address: member.address,
+						name: member.discord,
 					});
 				});
 				setChoices(mappedProfiles);
 			};
-			loadProfiles();
+			loadDiscordNames();
 		}
-	}, [proposal, activeTab]);
+	}, [proposal, activeTab, electionCount]);
 
 	useEffect(() => {
 		if (proposal && activeTab !== SPACE_KEY.COUNCIL) {
@@ -46,47 +50,51 @@ const Results: React.FC<ResultsProps> = ({ hash }) => {
 
 	if (proposal.isSuccess && proposal.data && choices) {
 		const { data } = proposal;
+
+		const totalVotes = data.totalVotesBalances !== 0 ? data.totalVotesBalances : 1;
+
+		const mappedResults = [] as any;
+
+		if (activeTab === SPACE_KEY.COUNCIL) {
+			data.totalBalances.forEach((balance, key) => {
+				mappedResults.push({
+					label: choices[key].name ? choices[key].name : choices[key].address,
+					balance: balance,
+				});
+			});
+		} else {
+			data.totalBalances.forEach((balance, key) => {
+				mappedResults.push({
+					label: choices[key],
+					balance: balance,
+				});
+			});
+		}
+
+		mappedResults.sort((a: any, b: any) => b.balance - a.balance);
+
 		return (
 			<MaxHeightColumn>
-				{choices.map((choice: any, i: number) => {
-					const votes = data.totalBalances[i] !== 0 ? data.totalBalances[i] : 0;
-					const totalVotes = data.totalVotesBalances !== 0 ? data.totalVotesBalances : 1;
-					if (activeTab === SPACE_KEY.COUNCIL) {
-						return (
-							<Row key={i}>
-								<StyledTooltip
-									arrow={true}
-									placement="bottom"
-									content={choice.ens ? choice.ens : choice.name ? choice.name : choice.address}
-									hideOnClick={false}
-								>
-									<Title>
-										<p>{choice.ens ? choice.ens : choice.name ? choice.name : choice.address} </p> (
-										{`${formatNumber(data.totalBalances[i])} ${data.spaceSymbol}`})
-									</Title>
-								</StyledTooltip>
-								<BarContainer>
-									<StyledProgressBar percentage={votes / totalVotes} variant="blue-pink" />
-									<Value>{formatPercent(votes / totalVotes)}</Value>
-								</BarContainer>
-							</Row>
-						);
-					} else {
-						return (
-							<Row key={i}>
-								<StyledTooltip arrow={true} placement="bottom" content={choice} hideOnClick={false}>
-									<Title>
-										<p>{choice} </p> ({`${formatNumber(data.totalBalances[i])} ${data.spaceSymbol}`}
-										)
-									</Title>
-								</StyledTooltip>
-								<BarContainer>
-									<StyledProgressBar percentage={votes / totalVotes} variant="blue-pink" />
-									<Value>{formatPercent(votes / totalVotes)}</Value>
-								</BarContainer>
-							</Row>
-						);
-					}
+				{mappedResults.map((choice: any, i: number) => {
+					return (
+						<Row key={i} highlight={activeTab === SPACE_KEY.COUNCIL && i < numOfCouncilSeats}>
+							<StyledTooltip
+								arrow={true}
+								placement="bottom"
+								content={choice.label}
+								hideOnClick={false}
+							>
+								<Title>
+									<Label>{choice.label} </Label> (
+									{`${formatNumber(choice.balance)} ${data.spaceSymbol}`})
+								</Title>
+							</StyledTooltip>
+							<BarContainer>
+								<StyledProgressBar percentage={choice.balance / totalVotes} variant="blue-pink" />
+								<Value>{formatPercent(choice.balance / totalVotes)}</Value>
+							</BarContainer>
+						</Row>
+					);
 				})}
 			</MaxHeightColumn>
 		);
@@ -102,11 +110,12 @@ const StyledSpinner = styled(Svg)`
 	margin: 30px auto;
 `;
 
-const Row = styled(FlexDivRowCentered)`
+const Row = styled(FlexDivRowCentered)<{ highlight: boolean }>`
 	border-bottom: 0.5px solid ${(props) => props.theme.colors.grayBlue};
 	justify-content: space-between;
 	padding: 8px;
 	margin: 8px 8px;
+	background: ${(props) => props.highlight && props.theme.colors.mediumBlue};
 `;
 
 const Title = styled.div`
@@ -119,14 +128,17 @@ const Title = styled.div`
 	align-items: center;
 	margin-left: 16px;
 
-	p {
-		text-overflow: ellipsis;
-		overflow: hidden;
-		white-space: nowrap;
-		width: 100px;
-	}
-
 	width: 250px;
+
+	padding: 16px 0px;
+`;
+
+const Label = styled.span`
+	margin-right: 4px;
+	text-overflow: ellipsis;
+	overflow: hidden;
+	white-space: nowrap;
+	width: 100px;
 `;
 
 const Value = styled(FlexDivRowCentered)`
