a876f005efc2d45ef9a994267bbfcd7220ea4597
Clement Balestrat <clement.balestrat@gmail.com>
2021-05-17 12:51:03 +1000

    removed L2 Mintr button

--- sections/layer2/index.tsx
+++ sections/layer2/index.tsx
@@ -88,11 +88,6 @@ const Index: FC = () => {
 				copy: t('layer2.actions.apr.subtitle'),
 				externalLink: EXTERNAL_LINKS.Synthetix.OEBlog,
 			},
-			goToMintr: {
-				title: t('layer2.actions.go-to-mintr.title'),
-				copy: t('layer2.actions.go-to-mintr.subtitle'),
-				externalLink: EXTERNAL_LINKS.Synthetix.MintrL2,
-			},
 		}),
 		[t, l2AmountSNX, l2APR]
 	);
@@ -114,10 +109,6 @@ const Index: FC = () => {
 							gridLocations: ['col-1', 'col-2', 'row-2', 'row-3'],
 							...ACTIONS.migrate,
 							isDisabled: stakingEscrow.isZero(),
-						},
-						{
-							gridLocations: ['col-2', 'col-3', 'row-2', 'row-3'],
-							...ACTIONS.goToMintr,
 						},
 				  ]
 				: [
