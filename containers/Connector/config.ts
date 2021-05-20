673fd23fe9c96b5b3341cecba3ef9e414585b449
Clement Balestrat <clement.balestrat@gmail.com>
2021-05-20 17:06:31 +1000

    safe config added

--- containers/Connector/config.ts
+++ containers/Connector/config.ts
@@ -1,4 +1,4 @@
-import onboard from 'bnc-onboard';
+import onboard from '@gnosis.pm/safe-apps-onboard';
 
 import { Subscriptions } from 'bnc-onboard/dist/src/interfaces';
 import { getInfuraRpcURL } from 'utils/infura';
