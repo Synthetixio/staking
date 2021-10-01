872d3dd55fe15d6600440cf6bf9211f9f5be3e4e
dbeal <git@dbeal.dev>
2021-10-01 09:42:27 -0700

    add thales staking information banner (#723)

--- constants/storage.ts
+++ constants/storage.ts
@@ -1,5 +1,6 @@
 export const LOCAL_STORAGE_KEYS = {
 	SELECTED_WALLET: 'selectedWallet',
 	WATCHED_WALLETS: 'watchedWallets',
+	THALES_STAKING_INFO_VISIBLE: 'thalesStakingInfo',
 	WARNING_URL_BANNER_VISIBLE: 'isWarningUrlBannerVisible',
 };
