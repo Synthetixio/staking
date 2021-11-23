3421ba0804cfd359be135ab3312de0932b190b69
Joachim Bachstätter <joachim.bachstatter@gmail.com>
2021-11-24 09:36:00 +1100

    Lint. - Sorry did previous comment commit thorugh github so prettier didn't run

--- sections/staking/components/BurnTab/BurnTab.tsx
+++ sections/staking/components/BurnTab/BurnTab.tsx
@@ -46,7 +46,7 @@ const BurnTab: FC = () => {
 		let etherNeededToBuy;
 		let sUSDNeededToBuy;
 		let sUSDNeededToBurn;
-		
+
 		// actualDebtToBurn gets displayed to the user when using max or clear
 		const actualDebtToBurn = debtBalance.gt(sUSDBalance) ? wei(sUSDBalance) : debtBalance;
 		// maxBurnAmount is whats gets passed to the smart contract, we do this to avoid dust when debt pool flucuates, burnSynths() will never burn more than the debt either way
