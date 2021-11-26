284e3c14a360be438b6b1ff76b4c2b5efbe33868
fritzschoff <max.j.f@gmx.de>
2021-11-26 11:01:43 +0100

    renamed and moved url function

--- sections/debt/components/ManageTab/index.tsx
+++ sections/debt/components/ManageTab/index.tsx
@@ -5,18 +5,20 @@ import { walletAddressState } from 'store/wallet';
 import styled from 'styled-components';
 import { FlexDivColCentered } from 'styles/common';
 
+const getDehedgeUniswapUrl = (debt: Wei) =>
+	`https://app.uniswap.org/#/swap?inputCurrency=0x57ab1ec28d129707052df4df418d58a2d46d5f51&exactAmount=${debt.toNumber()}&exactField=input`;
+
 const ManageTab = () => {
 	const walletAddress = useRecoilValue(walletAddressState);
 	const { debtBalance: actualDebt } = useUserStakingData(walletAddress);
 
-	const uniswapURLWithQueryParams = (debt: Wei) =>
-		`https://app.uniswap.org/#/swap?inputCurrency=0x57ab1ec28d129707052df4df418d58a2d46d5f51&exactAmount=${debt.toNumber()}&exactField=input`;
 	return (
 		<ManageContainer>
 			{!walletAddress ? (
 				<span>Please connect a wallet</span>
 			) : (
 				<iframe
+					title="uniswap-dhedge"
 					style={{
 						width: '100%',
 						height: 'auto',
@@ -26,7 +28,7 @@ const ManageTab = () => {
 						overflow: 'hidden',
 					}}
 					scrolling="no"
-					src={uniswapURLWithQueryParams(actualDebt)}
+					src={getDehedgeUniswapUrl(actualDebt)}
 				></iframe>
 			)}
 		</ManageContainer>
