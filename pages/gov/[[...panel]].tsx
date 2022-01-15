24924080ff9fd305eaa6067f9789eb3e4c86a91c
AndyTCF <andy.c.f.98@gmail.com>
2022-01-15 11:29:13 +1100

    remove unnessecary hook call

--- pages/gov/[[...panel]].tsx
+++ pages/gov/[[...panel]].tsx
@@ -9,52 +9,21 @@ import UIContainer from 'containers/UI';
 import StatsSection from 'components/StatsSection';
 import MainContent from 'sections/gov';
 import { snapshotEndpoint, SPACE_KEY } from 'constants/snapshot';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
-import { numOfCouncilSeatsState } from 'store/gov';
-import { ethers } from 'ethers';
-import Connector from 'containers/Connector';
-import councilDilution from 'contracts/councilDilution';
-import { appReadyState } from 'store/app';
+import { useRecoilValue } from 'recoil';
 import useSynthetixQueries from '@synthetixio/queries';
 import { walletAddressState } from 'store/wallet';
 
 const Gov: React.FC = () => {
 	const { t } = useTranslation();
-	const { provider } = Connector.useContainer();
 	const { setTitle } = UIContainer.useContainer();
-
 	const walletAddress = useRecoilValue(walletAddressState);
 	const { useVotingWeightQuery } = useSynthetixQueries();
-
 	const walletVotingWeight = useVotingWeightQuery(
 		snapshotEndpoint,
 		SPACE_KEY.COUNCIL,
 		walletAddress
 	);
 
-	const setNumOfCouncilSeats = useSetRecoilState(numOfCouncilSeatsState);
-	const isAppReady = useRecoilValue(appReadyState);
-
-	useEffect(() => {
-		if (isAppReady && provider) {
-			const getNumberOfCouncilSeats = async () => {
-				let contract = new ethers.Contract(
-					councilDilution.address,
-					councilDilution.abi,
-					provider as ethers.providers.Provider
-				);
-
-				const numOfCouncilMembersBN = await contract.numOfSeats();
-
-				const numOfCouncilMembers = Number(numOfCouncilMembersBN);
-				setNumOfCouncilSeats(numOfCouncilMembers);
-			};
-
-			getNumberOfCouncilSeats();
-		}
-	}, [isAppReady, provider, setNumOfCouncilSeats]);
-
-	// header title
 	useEffect(() => {
 		setTitle('gov');
 	}, [setTitle]);
