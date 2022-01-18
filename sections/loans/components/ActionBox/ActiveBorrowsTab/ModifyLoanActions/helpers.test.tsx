851efc3c55540930613356536540165b67102d54
Joachim Bachstätter <joachim.bachstatter@gmail.com>
2022-01-19 10:46:26 +1100

    Change SAFE_MIN_CRATIO_BUFFER to 0.1

--- sections/loans/components/ActionBox/ActiveBorrowsTab/ModifyLoanActions/helpers.test.tsx
+++ sections/loans/components/ActionBox/ActiveBorrowsTab/ModifyLoanActions/helpers.test.tsx
@@ -8,7 +8,7 @@ describe('calculateMaxDraw', () => {
 			collateralAsset: 'ETH',
 			currency: 'sUSD',
 			collateral: wei(1).toBN(),
-			amount: wei(1500).toBN(),
+			amount: wei(1600).toBN(),
 			minCratio: wei(1.3).toBN(),
 		} as any;
 		const exchangeRates = {
@@ -16,14 +16,14 @@ describe('calculateMaxDraw', () => {
 			[Synths.sUSD]: wei(1),
 		};
 		const result = calculateMaxDraw(loan, exchangeRates);
-		expect(result.toString(0)).toBe('500'); // 3000 / (1.3 + 0.2) - 1500
+		expect(result.toString(2)).toBe('542.86'); // 3000 / (1.3 + 0.1) - 1500
 	});
 	test('returns 0 if maxDraw is negative', () => {
 		const loan = {
 			collateralAsset: 'ETH',
 			currency: 'sUSD',
 			collateral: wei(1).toBN(),
-			amount: wei(2100).toBN(),
+			amount: wei(2500).toBN(),
 			minCratio: wei(1.3).toBN(),
 		} as any;
 		const exchangeRates = {
@@ -31,7 +31,7 @@ describe('calculateMaxDraw', () => {
 			[Synths.sUSD]: wei(1),
 		};
 		const result = calculateMaxDraw(loan, exchangeRates);
-		expect(result.toString(0)).toBe('0'); // 3000 / (1.3 + 0.2)
+		expect(result.toString(0)).toBe('0'); // 3000 / (1.3 + 0.1) - 2500 would be negative
 	});
 });
 
@@ -39,6 +39,6 @@ describe('getSafeCratio', () => {
 	test('works', () => {
 		const loan = { minCratio: wei(1.3) } as any;
 		const result = getSafeCratio(loan);
-		expect(result.toString(1)).toBe('1.5');
+		expect(result.toString(1)).toBe('1.4');
 	});
 });
