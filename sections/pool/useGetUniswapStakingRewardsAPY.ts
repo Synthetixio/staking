3abff53e88d87d4ba9920e62600245b8cb330d55
Joachim Bachstätter <joachim.bachstatter@gmail.com>
2022-01-24 11:58:40 +1100

    Add types and use wei for calculations

--- sections/pool/useGetUniswapStakingRewardsAPY.ts
+++ sections/pool/useGetUniswapStakingRewardsAPY.ts
@@ -1,10 +1,11 @@
 import { useQuery } from 'react-query';
-import { BigNumber, ethers, utils } from 'ethers';
+import { BigNumber, ethers } from 'ethers';
 import Connector from 'containers/Connector';
+import { wei } from '@synthetixio/wei';
 
-const ONE_YEAR = 365 * 24 * 3600;
+const ONE_YEAR_SECONDS = 365 * 24 * 3600;
 const GELATO_POOL_ADDRESS = '0x83bEeFB4cA39af649D03969B442c0E9F4E1732D8'; // WETH/SNX
-const GELTAO_POOL_ABI = [
+const GELATO_POOL_ABI = [
 	{
 		inputs: [],
 		name: 'getUnderlyingBalances',
@@ -37,6 +38,13 @@ type StakingRewardsData = {
 	eth: number;
 };
 
+type CoinGeckoSnxRates = { havven: { usd: number }; ethereum: { usd: number } };
+const getCoinGeckoSnxRates = async (): Promise<CoinGeckoSnxRates> => {
+	const resp = await fetch(
+		'https://api.coingecko.com/api/v3/simple/price?ids=havven%2Cethereum&vs_currencies=usd'
+	);
+	return resp.json();
+};
 export const useGetUniswapStakingRewardsAPY = ({
 	stakingRewardsContract,
 	tokenContract,
@@ -57,7 +65,7 @@ export const useGetUniswapStakingRewardsAPY = ({
 				if (provider) {
 					const GelatoPoolContract = new ethers.Contract(
 						GELATO_POOL_ADDRESS,
-						GELTAO_POOL_ABI,
+						GELATO_POOL_ABI,
 						provider
 					);
 					const [
@@ -67,32 +75,44 @@ export const useGetUniswapStakingRewardsAPY = ({
 						rewardForDuration,
 						duration,
 						contractBalance,
+					]: [
+						{ amount0Current: BigNumber; amount1Current: BigNumber },
+						BigNumber,
+						CoinGeckoSnxRates,
+						BigNumber,
+						BigNumber,
+						BigNumber
 					] = await Promise.all([
 						GelatoPoolContract.getUnderlyingBalances(),
 						GelatoPoolContract.totalSupply(),
-						fetch(
-							'https://api.coingecko.com/api/v3/simple/price?ids=havven%2Cethereum&vs_currencies=usd'
-						),
-						stakingRewardsContract?.connect(provider).getRewardForDuration(),
-						stakingRewardsContract?.connect(provider).rewardsDuration(),
-						tokenContract?.connect(provider).balanceOf(stakingRewardsContract?.address),
+						getCoinGeckoSnxRates(),
+						stakingRewardsContract?.connect(provider).getRewardForDuration() ??
+							Promise.resolve(BigNumber.from(0)),
+						stakingRewardsContract?.connect(provider).rewardsDuration() ??
+							Promise.resolve(BigNumber.from(0)),
+						tokenContract?.connect(provider).balanceOf(stakingRewardsContract?.address) ??
+							Promise.resolve(BigNumber.from(0)),
 					]);
 					const {
 						havven: { usd: snxRate },
 						ethereum: { usd: ethRate },
-					} = await ratesResults.json();
+					} = ratesResults;
 					const { amount0Current, amount1Current } = balances;
-					const totalValueInPool =
-						(amount0Current / 1e18) * ethRate + (amount1Current / 1e18) * snxRate;
-					const gUNIPrice = totalValueInPool / (gUNITotalSupply / 1e18);
-					const yearProRata = ONE_YEAR / Number(duration);
-					const gUNIValueInContract = (contractBalance / 1e18) * gUNIPrice;
-					const rewardsValuePerYear = (rewardForDuration / 1e18) * yearProRata * snxRate;
+					const amount0CurrentWei = wei(amount0Current);
+					const amount1CurrentWei = wei(amount1Current);
+					const totalValueInPool = amount0CurrentWei
+						.mul(ethRate)
+						.add(amount1CurrentWei.mul(snxRate));
+
+					const gUNIPrice = wei(totalValueInPool).div(wei(gUNITotalSupply));
+					const yearProRata = wei(ONE_YEAR_SECONDS).div(wei(duration));
+					const gUNIValueInContract = wei(contractBalance).mul(gUNIPrice);
+					const rewardsValuePerYear = wei(rewardForDuration).mul(yearProRata).mul(snxRate);
 
 					return {
-						eth: amount0Current / 1e18,
-						snx: amount1Current / 1e18,
-						apy: (100 * rewardsValuePerYear) / gUNIValueInContract,
+						eth: amount0CurrentWei.toNumber(),
+						snx: amount1CurrentWei.toNumber(),
+						apy: wei(100).mul(rewardsValuePerYear).div(gUNIValueInContract).toNumber(),
 					};
 				}
 				return {
