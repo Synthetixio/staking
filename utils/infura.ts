696746be7ae0d2f5fbc9ad79c5a32ab4889dadf4
Joachim Bachstätter <joachim.bachstatter@gmail.com>
2022-01-28 12:57:58 +1100

    Correct url for infura. Infure expects optimism- at the start and not -ovm at the end

--- utils/infura.ts
+++ utils/infura.ts
@@ -5,7 +5,10 @@ export const GWEI_DECIMALS = 9;
 export const GWEI_UNIT = 1000000000;
 
 export const getInfuraRpcURL = (network?: Network | null) => {
-	const networkName = network?.name ? network.name : `mainnet`;
-	const url = `https://${networkName}.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_PROJECT_ID}`;
+	const networkName = (network?.name ? network.name : `mainnet`).replace('-ovm', '');
+	const optimismPrefix = network?.useOvm ? 'optimism-' : '';
+	const url = `https://${optimismPrefix + networkName}.infura.io/v3/${
+		process.env.NEXT_PUBLIC_INFURA_PROJECT_ID
+	}`;
 	return url;
 };
