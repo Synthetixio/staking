d53a67e54bf1b93251d9a6245e427423350339af
Joachim Bachstätter <joachim.bachstatter@gmail.com>
2022-02-04 12:10:32 +1100

    Remove unnecessary useEffect +state - This make sure the debt stats update when changing wallet/ network - Also refactored forEach to a .map

--- hooks/useHistoricalDebtData.ts
+++ hooks/useHistoricalDebtData.ts
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { last } from 'lodash';
 import useSynthetixQueries from '@synthetixio/queries';
 import Wei, { wei } from '@synthetixio/wei';
@@ -16,12 +15,7 @@ type HistoricalDebtAndIssuance = {
 	data: HistoricalDebtAndIssuanceData[] | [];
 };
 
-const useHistoricalDebtData = (walletAddress: string | null) => {
-	const [historicalDebt, setHistoricalDebt] = useState<HistoricalDebtAndIssuance>({
-		isLoading: true,
-		data: [],
-	});
-
+const useHistoricalDebtData = (walletAddress: string | null): HistoricalDebtAndIssuance => {
 	const { useGetDebtDataQuery, subgraph } = useSynthetixQueries();
 
 	const issues = subgraph.useGetIssueds(
@@ -57,59 +51,54 @@ const useHistoricalDebtData = (walletAddress: string | null) => {
 
 	const isLoaded =
 		issues.isSuccess && burns.isSuccess && debtSnapshot.isSuccess && debtDataQuery.isSuccess;
+	if (!isLoaded) {
+		return { isLoading: true, data: [] };
+	}
 
-	useEffect(() => {
-		if (isLoaded) {
-			let issuesAndBurns = issues.data!.map((b) => ({ isBurn: false, ...b }));
-			issuesAndBurns = sortBy(
-				issuesAndBurns.concat(burns.data!.map((b) => ({ isBurn: true, ...b }))),
-				(d) => d.timestamp.toNumber()
-			);
+	let issuesAndBurns = issues.data?.map((b) => ({ isBurn: false, ...b })) || [];
+	issuesAndBurns = sortBy(
+		issuesAndBurns.concat(burns.data!.map((b) => ({ isBurn: true, ...b }))),
+		(d) => d.timestamp.toNumber()
+	);
 
-			const debtHistory = debtSnapshot.data ?? [];
+	const debtHistory = debtSnapshot.data ?? [];
 
-			// We set historicalIssuanceAggregation array, to store all the cumulative
-			// values of every mint and burns
-			const historicalIssuanceAggregation: Wei[] = [];
+	// We set historicalIssuanceAggregation array, to store all the cumulative
+	// values of every mint and burns
+	const historicalIssuanceAggregation: Wei[] = [];
 
-			issuesAndBurns.slice().forEach((event) => {
-				const multiplier = event.isBurn ? -1 : 1;
-				const aggregation = event.value
-					.mul(multiplier)
-					.add(last(historicalIssuanceAggregation) ?? wei(0));
+	issuesAndBurns.slice().forEach((event) => {
+		const multiplier = event.isBurn ? -1 : 1;
+		const aggregation = event.value
+			.mul(multiplier)
+			.add(last(historicalIssuanceAggregation) ?? wei(0));
 
-				historicalIssuanceAggregation.push(aggregation);
-			});
+		historicalIssuanceAggregation.push(aggregation);
+	});
 
-			// We merge both actual & issuance debt into an array
-			let historicalDebtAndIssuance: HistoricalDebtAndIssuanceData[] = [];
-			debtHistory
-				.slice()
-				.reverse()
-				.forEach((debtSnapshot, i) => {
-					historicalDebtAndIssuance.push({
-						timestamp: debtSnapshot.timestamp.toNumber() * 1000,
-						issuanceDebt: historicalIssuanceAggregation[i],
-						actualDebt: wei(debtSnapshot.debtBalanceOf || 0),
-						index: i,
-					});
-				});
+	// We merge both actual & issuance debt into an array
+	const historicalDebtAndIssuance: HistoricalDebtAndIssuanceData[] = debtHistory
+		.slice()
+		.reverse()
+		.map((debtSnapshot, i) => {
+			return {
+				timestamp: debtSnapshot.timestamp.toNumber() * 1000,
+				issuanceDebt: historicalIssuanceAggregation[i],
+				actualDebt: wei(debtSnapshot.debtBalanceOf || 0),
+				index: i,
+			};
+		});
 
-			// Last occurrence is the current state of the debt
-			// Issuance debt = last occurrence of the historicalDebtAndIssuance array
-			historicalDebtAndIssuance.push({
-				timestamp: new Date().getTime(),
-				actualDebt: debtDataQuery.data?.debtBalance || wei(0),
-				issuanceDebt: last(historicalIssuanceAggregation) ?? wei(0),
-				index: historicalDebtAndIssuance.length,
-			});
+	// Last occurrence is the current state of the debt
+	// Issuance debt = last occurrence of the historicalDebtAndIssuance array
+	const data = historicalDebtAndIssuance.concat({
+		timestamp: new Date().getTime(),
+		actualDebt: debtDataQuery.data?.debtBalance || wei(0),
+		issuanceDebt: last(historicalIssuanceAggregation) ?? wei(0),
+		index: historicalDebtAndIssuance.length,
+	});
 
-			setHistoricalDebt({ isLoading: false, data: historicalDebtAndIssuance });
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [isLoaded]);
-
-	return historicalDebt;
+	return { isLoading: false, data };
 };
 
 export default useHistoricalDebtData;
