02f7a2612773c075b2a885b4c71f6902f16b0655
fritzschoff <max.j.f@gmx.de>
2022-02-07 11:32:32 +0100

    removed commented out components

--- sections/debt/components/DebtHedgingInfoPanel/DebtHedgingInfoPanel.tsx
+++ sections/debt/components/DebtHedgingInfoPanel/DebtHedgingInfoPanel.tsx
@@ -13,28 +13,29 @@ const DebtHedgingInfoPanel: React.FC<DebtHedgingInfoPanelProps> = ({ hidden }) =
 
 	if (hidden) return null;
 	return (
-		<span></span>
-		/* 	<InfoPanelContainer>
-			<InfoPanelTitle>{t('debt.actions.manage.info-panel.title')}</InfoPanelTitle>
-			<InfoPanelBody>
-				<Trans
-					i18nKey="debt.actions.manage.info-panel.body"
-					components={[
-						<ExternalLink href="https://docs.dhedge.org/dhedge-original-pools/v2-snx-debt-mirror" />,
-					]}
-				/>
-				<br />
-				<StyledUniswapLinkWrapper>
-					Warning: dSNX approximately hedges debt, users can incur losses. Be mindful of price
-					impact and use&nbsp;
-					<StyledUniswapLink href="https://info.uniswap.org/#/pools/0x9957c4795ab663622db54fc48fda874da59150ff">
-						this uniswap pool&nbsp;
-					</StyledUniswapLink>
-					to close dSNX positions.
-				</StyledUniswapLinkWrapper>
-			</InfoPanelBody>
-			<DebtHedgingChart />
-		</InfoPanelContainer> */
+		<>
+			<InfoPanelContainer>
+				<InfoPanelTitle>{t('debt.actions.manage.info-panel.title')}</InfoPanelTitle>
+				<InfoPanelBody>
+					<Trans
+						i18nKey="debt.actions.manage.info-panel.body"
+						components={[
+							<ExternalLink href="https://docs.dhedge.org/dhedge-original-pools/v2-snx-debt-mirror" />,
+						]}
+					/>
+					<br />
+					<StyledUniswapLinkWrapper>
+						Warning: dSNX approximately hedges debt, users can incur losses. Be mindful of price
+						impact and use&nbsp;
+						<StyledUniswapLink href="https://info.uniswap.org/#/pools/0x9957c4795ab663622db54fc48fda874da59150ff">
+							this uniswap pool&nbsp;
+						</StyledUniswapLink>
+						to close dSNX positions.
+					</StyledUniswapLinkWrapper>
+				</InfoPanelBody>
+				<DebtHedgingChart />
+			</InfoPanelContainer>
+		</>
 	);
 };
 
