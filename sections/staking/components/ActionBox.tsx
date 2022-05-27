fee330edf799c0c60767c9a28f3387947667fd79
bachstatter <joey@cc.snxdao.io>
2022-05-27 13:48:18 +1000

    Use the new SelfLiquidateTab Only render the tab if: 1.  User is staking 2. User c-ration is below target c-ratio 3. User sUSD balance is less than burn amount to fix c-ratio 4. it's not a delegate wallet

--- sections/staking/components/ActionBox.tsx
+++ sections/staking/components/ActionBox.tsx
@@ -9,8 +9,16 @@ import BurnTab from './BurnTab';
 import MintTab from './MintTab';
 import Burn from 'assets/svg/app/burn.svg';
 import Mint from 'assets/svg/app/mint.svg';
-import { useSetRecoilState } from 'recoil';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
 import { burnTypeState, StakingPanelType, mintTypeState } from 'store/staking';
+import SelfLiquidateTab from './SelfLiquidateTab';
+import useStakingCalculations from '../hooks/useStakingCalculations';
+import { notNill } from 'utils/ts-helpers';
+import ROUTES from 'constants/routes';
+import useSynthetixQueries from '@synthetixio/queries';
+import { delegateWalletState, walletAddressState } from 'store/wallet';
+import Wei, { wei } from '@synthetixio/wei';
+import { Synths } from 'constants/currency';
 
 type ActionBoxProps = {
 	currentTab: string;
@@ -21,6 +29,20 @@ const ActionBox: FC<ActionBoxProps> = ({ currentTab }) => {
 	const router = useRouter();
 	const onMintTypeChange = useSetRecoilState(mintTypeState);
 	const onBurnTypeChange = useSetRecoilState(burnTypeState);
+	const walletAddress = useRecoilValue(walletAddressState);
+	const delegateWallet = useRecoilValue(delegateWalletState);
+	const { useSynthsBalancesQuery } = useSynthetixQueries();
+
+	const {
+		percentageCurrentCRatio,
+		percentageTargetCRatio,
+		isLoading,
+		debtBalance,
+		issuableSynths,
+	} = useStakingCalculations();
+	const synthsBalancesQuery = useSynthsBalancesQuery(walletAddress, { staleTime: 5000 });
+	const sUSDBalance = synthsBalancesQuery?.data?.balancesMap[Synths.sUSD]?.balance ?? wei(0);
+	const burnAmountToFixCRatio = wei(Wei.max(debtBalance.sub(issuableSynths), wei(0)));
 
 	useEffect(() => {
 		if (currentTab === StakingPanelType.MINT) {
@@ -30,24 +52,49 @@ const ActionBox: FC<ActionBoxProps> = ({ currentTab }) => {
 		}
 	}, [currentTab, onBurnTypeChange, onMintTypeChange]);
 
+	const showSelfLiquidationTab =
+		percentageCurrentCRatio.gt(0) &&
+		percentageCurrentCRatio.lt(percentageTargetCRatio) &&
+		sUSDBalance.lt(burnAmountToFixCRatio) &&
+		delegateWallet?.address === undefined;
+
+	useEffect(() => {
+		if (isLoading) return;
+
+		const isOnSelLiquidateTab = router.query?.action?.[0] === StakingPanelType.SELF_LIQUIDATE;
+		if (isOnSelLiquidateTab && !showSelfLiquidationTab) {
+			// If user is on the self liquidate tab and isn't staking, navigate back staking home tab
+			router.replace(ROUTES.Staking.Home);
+		}
+	});
 	const tabData = useMemo(
-		() => [
-			{
-				title: t('staking.actions.mint.title'),
-				icon: <Svg src={Mint} />,
-				tabChildren: <MintTab />,
-				blue: true,
-				key: StakingPanelType.MINT,
-			},
-			{
-				title: t('staking.actions.burn.title'),
-				icon: <Svg src={Burn} />,
-				tabChildren: <BurnTab />,
-				blue: false,
-				key: StakingPanelType.BURN,
-			},
-		],
-		[t]
+		() =>
+			[
+				{
+					title: t('staking.actions.mint.title'),
+					icon: <Svg src={Mint} />,
+					tabChildren: <MintTab />,
+					blue: true,
+					key: StakingPanelType.MINT,
+				},
+				{
+					title: t('staking.actions.burn.title'),
+					icon: <Svg src={Burn} />,
+					tabChildren: <BurnTab />,
+					blue: false,
+					key: StakingPanelType.BURN,
+				},
+				showSelfLiquidationTab
+					? {
+							title: 'Self Liquidate',
+							icon: <Svg src={Burn} />,
+							tabChildren: <SelfLiquidateTab />,
+							blue: false,
+							key: StakingPanelType.SELF_LIQUIDATE,
+					  }
+					: undefined,
+			].filter(notNill),
+		[showSelfLiquidationTab, t]
 	);
 
 	return (
