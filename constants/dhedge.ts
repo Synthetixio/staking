4a64c5085bf78e8d7934f4430222b9f39e236a45
bachstatter <joey@cc.snxdao.io>
2022-06-03 14:19:07 +1000

    Move dHedge contract to dhedge contract file and add mainnet suffix - Also add dSNXContractOptimism

--- constants/dhedge.ts
+++ constants/dhedge.ts
@@ -1,1 +1,13 @@
+import { Contract } from 'ethers';
+import { abi as erc20ABI } from 'contracts/erc20';
+
 export const dHedgeAPIUrl = 'https://api-v2.dhedge.org/graphql';
+export const dSNXContractMainnet = new Contract(
+	'0x5f7F94a1dd7b15594d17543BEB8B30b111DD464c',
+	erc20ABI
+);
+export const dSNXContractOptimism = new Contract(
+	'0x59babc14dd73761e38e5bda171b2298dc14da92d',
+	erc20ABI
+);
+
