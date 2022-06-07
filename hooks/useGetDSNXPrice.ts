ac17be968b0a0af3e6302b866c02ae5be233fbd2
bachstatter <joey@cc.snxdao.io>
2022-06-07 15:20:44 +1000

    Add useGetDSNXPrice hook

---
+++ hooks/useGetDSNXPrice.ts
@@ -0,0 +1,27 @@
+import { useQuery, UseQueryOptions } from 'react-query';
+
+import { dSNXPoolAddressOptimism } from 'constants/dhedge';
+import Connector from 'containers/Connector';
+import { Contract } from 'ethers';
+import { abi } from 'contracts/dHedgePoolLogic';
+import Wei, { wei } from '@synthetixio/wei';
+
+const useGetDSNXPrice = (walletAddress: string | null, queryOptions?: UseQueryOptions<Wei>) => {
+	const { provider, network } = Connector.useContainer();
+	const contract = new Contract(dSNXPoolAddressOptimism, abi);
+	return useQuery(
+		[walletAddress, network?.name],
+		async () => {
+			if (!walletAddress) return wei(0);
+			if (!provider) return wei(0);
+			if (!network?.name) return wei(0);
+			const price = await contract.connect(provider).tokenPrice();
+			return wei(price);
+		},
+		{
+			enabled: Boolean(walletAddress && network?.name && provider),
+			...queryOptions,
+		}
+	);
+};
+export default useGetDSNXPrice;
