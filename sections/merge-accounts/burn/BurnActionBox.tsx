7e4db530a154960aa1aa687adce5d8f9bf74fb88
jmzwar <james.zwar@protonmail.com>
2022-06-12 21:38:23 +1000

    lint remove

--- sections/merge-accounts/burn/BurnActionBox.tsx
+++ sections/merge-accounts/burn/BurnActionBox.tsx
@@ -1,5 +1,5 @@
 import { useMemo, useEffect, FC, useCallback } from 'react';
-import styled, { useTheme } from 'styled-components';
+import styled from 'styled-components';
 import { Trans, useTranslation } from 'react-i18next';
 import { Svg } from 'react-optimized-image';
 import { useRouter } from 'next/router';
