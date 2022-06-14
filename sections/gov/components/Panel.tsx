4b99e4f0598eea1bbcfb3e642efe3805dd024457
bachstatter <joey@cc.snxdao.io>
2022-06-14 20:26:40 +1000

    Remove election table and move fetching of proposal to proposal component

--- sections/gov/components/Panel.tsx
+++ sections/gov/components/Panel.tsx
@@ -6,107 +6,23 @@ import { useRecoilState } from 'recoil';
 import { snapshotEndpoint, SPACE_KEY } from 'constants/snapshot';
 import { panelState, PanelType, proposalState } from 'store/gov';
 
-import useActiveTab from '../hooks/useActiveTab';
-import { Grid, Col, Spacer } from 'sections/gov/components/common';
+import { Grid, Col } from 'sections/gov/components/common';
 
-import StructuredTab from 'components/StructuredTab';
 import UnstructuredTab from 'components/UnstructuredTab';
 import CouncilBoard from './List/CouncilBoard';
 import Proposal from './Proposal';
 import List from './List';
 import Create from './Create';
-import { Proposal as ProposalType } from '@synthetixio/queries';
-import request, { gql } from 'graphql-request';
 import ROUTES from 'constants/routes';
 
 type PanelProps = {
 	currentTab: string;
 };
 
-const Panel: React.FC<PanelProps> = () => {
+const Panel = () => {
 	const { t } = useTranslation();
 	const router = useRouter();
-
-	const [, setProposal] = useRecoilState(proposalState);
-	const [panelType, setPanelType] = useRecoilState(panelState);
-	const { activeTab } = useActiveTab();
-
-	const fetchPreloadedProposal = useCallback(() => {
-		const fetch = async () => {
-			const hash = router && router.query.panel ? router.query?.panel[1] : '';
-			const { proposal }: { proposal: ProposalType } = await request(
-				snapshotEndpoint,
-				gql`
-					query Proposals($id: String) {
-						proposal(id: $id) {
-							id
-							title
-							body
-							choices
-							start
-							end
-							snapshot
-							state
-							author
-							space {
-								id
-								name
-							}
-						}
-					}
-				`,
-				{ id: hash }
-			);
-
-			setProposal(proposal);
-			setPanelType(PanelType.PROPOSAL);
-		};
-		fetch();
-	}, [router, setPanelType, setProposal]);
-
-	useEffect(() => {
-		if (Array.isArray(router.query.panel) && router.query.panel[1]) {
-			if (router.query.panel[1] === 'create') {
-				setPanelType(PanelType.CREATE);
-			} else {
-				fetchPreloadedProposal();
-			}
-		} else {
-			setPanelType(PanelType.LIST);
-			setProposal(null);
-		}
-	}, [router, fetchPreloadedProposal, setPanelType, setProposal]);
-
-	const tabData = useMemo(
-		() => [
-			{
-				title: t('gov.panel.council.title'),
-				description: t('gov.panel.council.description'),
-				tabChildren: <List spaceKey={activeTab} />,
-				key: SPACE_KEY.COUNCIL,
-			},
-			{
-				title: t('gov.panel.treasury-council.title'),
-				description: t('gov.panel.treasury-council.description'),
-				tabChildren: <List spaceKey={activeTab} />,
-				key: SPACE_KEY.TREASURY,
-			},
-			{
-				title: t('gov.panel.grants.title'),
-				description: t('gov.panel.grants.description'),
-				tabChildren: <List spaceKey={activeTab} />,
-				key: SPACE_KEY.GRANTS,
-			},
-			{
-				title: t('gov.panel.ambassador.title'),
-				description: t('gov.panel.ambassador.description'),
-				tabChildren: <List spaceKey={activeTab} />,
-				key: SPACE_KEY.AMBASSADOR,
-			},
-		],
-		[t, activeTab]
-	);
-
+	
 	const proposalsData = useMemo(
 		() => ({
 			title: t('gov.panel.proposals.title'),
@@ -118,67 +34,35 @@ const Panel: React.FC<PanelProps> = () => {
 
 		[t]
 	);
-
-	const returnContent = () => {
-		switch (panelType) {
-			case PanelType.PROPOSAL:
-				return (
-					<Proposal
-						onBack={() => {
-							setProposal(null);
-							setPanelType(PanelType.LIST);
-							if (window.history.length < 3) {
-								router.push(
-									ROUTES.Gov.Space(
-										activeTab === SPACE_KEY.AMBASSADOR ? SPACE_KEY.COUNCIL : activeTab
-									)
-								);
-							} else {
-								router.back();
-							}
-						}}
-					/>
-				);
-			case PanelType.CREATE:
-				return (
-					<Create
-						onBack={() => {
-							setPanelType(PanelType.LIST);
-							if (window.history.length < 3) {
-								router.push(
-									ROUTES.Gov.Space(
-										activeTab === SPACE_KEY.AMBASSADOR ? SPACE_KEY.COUNCIL : activeTab
-									)
-								);
-							} else {
-								router.back();
-							}
-						}}
-					/>
-				);
-			default:
-				return (
-					<Grid>
-						<Col>
-							<StructuredTab
-								boxPadding={5}
-								boxHeight={200}
-								tabData={tabData}
-								setActiveTab={(key) => router.push(`/gov/${key}`)}
-								activeTab={activeTab}
-							/>
-							<Spacer />
-							<UnstructuredTab tabData={proposalsData} />
-						</Col>
-						<Col>
-							<CouncilBoard />
-						</Col>
-					</Grid>
-				);
-		}
-	};
-
-	return returnContent();
+	switch (panelType) {
+		case PanelType.PROPOSAL:
+			return (
+				<Proposal
+					onBack={() => {
+						router.push(ROUTES.Gov.Home);
+					}}
+				/>
+			);
+		case PanelType.CREATE:
+			return (
+				<Create
+					onBack={() => {
+						router.push(ROUTES.Gov.Home);
+					}}
+				/>
+			);
+		default:
+			return (
+				<Grid>
+					<Col>
+						<UnstructuredTab tabData={proposalsData} />
+					</Col>
+					<Col>
+						<CouncilBoard />
+					</Col>
+				</Grid>
+			);
+	}
 };
 
 export default Panel;
