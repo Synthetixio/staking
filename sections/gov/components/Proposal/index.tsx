db2bfae8091e91890d4c3511d8cfa7f58e991610
bachstatter <joey@cc.snxdao.io>
2022-06-14 20:26:40 +1000

    Remove election table and move fetching of proposal to proposal component

--- sections/gov/components/Proposal/index.tsx
+++ sections/gov/components/Proposal/index.tsx
@@ -8,15 +8,58 @@ import { proposalState } from 'store/gov';
 import { useRecoilValue } from 'recoil';
 import Details from './Details';
 import Info from './Info';
+import { useQuery } from 'react-query';
+import { snapshotEndpoint } from 'constants/snapshot';
+import request, { gql } from 'graphql-request';
+import { useRouter } from 'next/router';
+import { Proposal as ProposalType } from '@synthetixio/queries';
 
 type ProposalProps = {
 	onBack: Function;
 };
 
+const useGetProposal = (hash?: string) => {
+	return useQuery(
+		['gov', 'proposal', hash],
+		async () => {
+			const { proposal }: { proposal: ProposalType } = await request(
+				snapshotEndpoint,
+				gql`
+					query Proposals($id: String) {
+						proposal(id: $id) {
+							id
+							title
+							body
+							choices
+							start
+							end
+							snapshot
+							state
+							author
+							space {
+								id
+								name
+							}
+						}
+					}
+				`,
+				{ id: hash }
+			);
+			return proposal;
+		},
+		{
+			enabled: Boolean(hash),
+			staleTime: 3.6e6, // 1hour
+		}
+	);
+};
 const Index: React.FC<ProposalProps> = ({ onBack }) => {
 	const { activeTab } = useActiveTab();
-	const proposal = useRecoilValue(proposalState);
+	const router = useRouter();
 
+	const hash = router && router.query.panel ? router.query?.panel[1] : undefined;
+	const proposalQuery = useGetProposal(hash);
+	const proposal = proposalQuery.data;
 	if (!proposal) return <></>;
 	return (
 		<Grid>
