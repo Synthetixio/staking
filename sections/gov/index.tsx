9b580e1f2a939005542dab28a41f356240448508
bachstatter <joey@cc.snxdao.io>
2022-06-14 20:57:03 +1000

    No need to set default tab

--- sections/gov/index.tsx
+++ sections/gov/index.tsx
@@ -1,13 +1,8 @@
 import { FC } from 'react';
-import { useRouter } from 'next/router';
 import Panel from 'sections/gov/components/Panel';
-import { SPACE_KEY } from 'constants/snapshot';
 
 const Index: FC = () => {
-	const router = useRouter();
-	const defaultTab = (router.query.panel && router.query.panel[0]) || SPACE_KEY.COUNCIL;
-
-	return <Panel currentTab={defaultTab} />;
+	return <Panel />;
 };
 
 export default Index;
