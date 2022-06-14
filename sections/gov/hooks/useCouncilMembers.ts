9f37611a8ee10fafec512da95192f399a5c41384
bachstatter <joey@cc.snxdao.io>
2022-06-14 20:56:21 +1000

    Refactor useCouncilMembers to react-query

--- sections/gov/hooks/useCouncilMembers.ts
+++ sections/gov/hooks/useCouncilMembers.ts
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 import spartanCouncil from 'contracts/spartanCouncil';
 import councilDilution from 'contracts/councilDilution';
@@ -6,65 +5,57 @@ import Connector from 'containers/Connector';
 import { useRecoilValue } from 'recoil';
 import { appReadyState } from 'store/app';
 import { getProfiles } from '../components/helper';
+import { useQuery } from 'react-query';
 
 export const useCouncilMembers = () => {
-	const [councilMembers, setCouncilMembers] = useState<any[] | null>(null);
-	const { provider } = Connector.useContainer();
+	const { provider, L2DefaultProvider, L1DefaultProvider } = Connector.useContainer();
 	const isAppReady = useRecoilValue(appReadyState);
+	return useQuery(
+		['gov', 'council-members'],
+		async () => {
+			let spartanCouncilContract = new ethers.Contract(
+				spartanCouncil.address,
+				spartanCouncil.abi,
+				L1DefaultProvider
+			);
 
-	useEffect(() => {
-		const getCouncilMembers = async () => {
-			if (provider && isAppReady) {
-				const L1Provider = new ethers.providers.InfuraProvider(1, process.env.INFURA_ARCHIVE_KEY);
+			let councilDilutionContract = new ethers.Contract(
+				councilDilution.address,
+				councilDilution.abi,
+				L2DefaultProvider
+			);
 
-				let spartanCouncilContract = new ethers.Contract(
-					spartanCouncil.address,
-					spartanCouncil.abi,
-					L1Provider
-				);
+			let councilMembers = [] as string[];
 
-				const ovmProvider = new ethers.providers.JsonRpcProvider('https://mainnet.optimism.io');
+			const numOfCouncilMembersBN = await councilDilutionContract.numOfSeats();
 
-				let councilDilutionContract = new ethers.Contract(
-					councilDilution.address,
-					councilDilution.abi,
-					ovmProvider
-				);
+			const numOfCouncilMembers = Number(numOfCouncilMembersBN);
 
-				let councilMembers = [] as any;
-
-				const numOfCouncilMembersBN = await councilDilutionContract.numOfSeats();
-
-				const numOfCouncilMembers = Number(numOfCouncilMembersBN);
-
-				for (let i = 1; i <= numOfCouncilMembers; i++) {
-					let address = await spartanCouncilContract.ownerOf(i);
-					if (address !== ethers.constants.AddressZero) {
-						councilMembers.push(ethers.utils.getAddress(address));
-					}
+			for (let i = 1; i <= numOfCouncilMembers; i++) {
+				let address = await spartanCouncilContract.ownerOf(i);
+				if (address !== ethers.constants.AddressZero) {
+					councilMembers.push(ethers.utils.getAddress(address));
 				}
-				let resolvedMembers = await Promise.resolve(councilMembers);
-
-				const profiles = await getProfiles(resolvedMembers);
-
-				const profileArray = Object.keys(profiles).map((profile) => {
-					const accessor = profiles[profile];
-					return {
-						ens: accessor.ens.length > 0 ? accessor.ens : null,
-						address: profile,
-						name: accessor.name ? accessor.name : null,
-					};
-				});
-
-				setCouncilMembers(profileArray);
-			} else {
-				setCouncilMembers(null);
 			}
-		};
-		getCouncilMembers();
-	}, [provider, isAppReady]);
+			let resolvedMembers = await Promise.resolve(councilMembers);
 
-	return councilMembers;
+			const profiles = await getProfiles(resolvedMembers);
+
+			const profileArray = Object.keys(profiles).map((profile) => {
+				const accessor = profiles[profile];
+				return {
+					ens: accessor.ens.length > 0 ? accessor.ens : null,
+					address: profile,
+					name: accessor.name ? accessor.name : null,
+				};
+			});
+			return profileArray;
+		},
+		{
+			staleTime: 600000, // 10 min stale time,
+			enabled: Boolean(provider && isAppReady && L1DefaultProvider && L2DefaultProvider),
+		}
+	);
 };
 
 export default useCouncilMembers;
