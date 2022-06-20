316f5f0adaddddbd5b474a9d31ae707f97da4db1
jmzwar <james.zwar@protonmail.com>
2022-06-21 09:25:20 +1000

    update register fn

--- hooks/useServiceWorker.ts
+++ hooks/useServiceWorker.ts
@@ -1,22 +1,23 @@
 import { useEffect } from 'react';
 
+const swInit = () => {
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.register('/sw.js').then(
+			function (registration) {
+				console.log('Service Worker registration successful with scope: ', registration.scope);
+			},
+			function (err) {
+				console.log('Service Worker registration failed: ', err);
+			}
+		);
+	}
+};
+
 export default function useServiceWorker() {
 	useEffect(() => {
-		if ('serviceWorker' in navigator) {
-			window.addEventListener('load', function () {
-				navigator.serviceWorker.register('/sw.js').then(
-					function (registration) {
-						console.log('Service Worker registration successful with scope: ', registration.scope);
-					},
-					function (err) {
-						console.log('Service Worker registration failed: ', err);
-					}
-				);
-			});
-		}
-
+		window.addEventListener('load', swInit);
 		return () => {
-			window.removeEventListener('load', () => {});
+			window.removeEventListener('load', swInit);
 		};
 	}, []);
 }
