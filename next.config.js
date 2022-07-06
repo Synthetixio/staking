ba20ad0d919d2eab14202725b0e3038a45ee2097
Nikita <nikita@cc.snxdao.io>
2022-07-06 15:14:15 +0800

    Optimise contracts by cleaning up deployments JSON

--- next.config.js
+++ next.config.js
@@ -1,20 +1,50 @@
-// next.config.js
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
+function optimiseContracts(config, { webpack }) {
+	const networks = ['kovan', 'kovan-ovm', 'mainnet', 'mainnet-ovm'];
+	const generate = require('./scripts/minify-synthetix-contract');
+	const out = require('path').resolve(__dirname, '.next/tmp');
+	require('fs').mkdirSync(out, { recursive: true });
+	generate({ networks, out });
+
+	networks.forEach((network) =>
+		config.plugins.push(
+			new webpack.NormalModuleReplacementPlugin(
+				new RegExp(`/synthetix/publish/deployed/${network}/deployment.json`),
+				require.resolve(`${out}/${network}.json`)
+			)
+		)
+	);
+	config.plugins.push(
+		new webpack.NormalModuleReplacementPlugin(
+			new RegExp(`/synthetix/publish/deployed/(goerli|local)`),
+			require.resolve('./scripts/noop')
+		)
+	);
+	config.plugins.push(
+		new webpack.NormalModuleReplacementPlugin(/^synthetix$/, require.resolve('synthetix/index.js'))
+	);
+}
+
+function generateBundleReport(config) {
+	if (process.env.GENERATE_BUNDLE_REPORT === 'true') {
+		const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+		const plugin = new BundleAnalyzerPlugin({
+			analyzerMode: 'static',
+			reportFilename: 'reports/webpack.html',
+			openAnalyzer: false,
+			generateStatsFile: false,
+		});
+		config.plugins.push(plugin);
+	}
+}
+
 module.exports = withPlugins([[optimizedImages, { images: { optimize: false } }]], {
-	webpack: (config) => {
+	webpack: (config, context) => {
 		config.resolve.mainFields = ['module', 'browser', 'main'];
-		if (process.env.GENERATE_BUNDLE_REPORT === 'true') {
-			const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-			const plugin = new BundleAnalyzerPlugin({
-				analyzerMode: 'static',
-				reportFilename: 'reports/webpack.html',
-				openAnalyzer: false,
-				generateStatsFile: false,
-			});
-			config.plugins.push(plugin);
-		}
+		generateBundleReport(config, context);
+		optimiseContracts(config, context);
 		return config;
 	},
 	trailingSlash: !!process.env.NEXT_PUBLIC_TRAILING_SLASH_ENABLED,
