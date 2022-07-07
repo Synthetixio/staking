0187907ec02c38f0bf6fba05ad43267e9c464295
Nikita <nikita@cc.snxdao.io>
2022-07-07 13:35:28 +0800

    Switch to Next's own bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.GENERATE_BUNDLE_REPORT === 'true',
+});
+
 function optimiseContracts(config, { webpack }) {
 	const networks = ['kovan', 'kovan-ovm', 'mainnet', 'mainnet-ovm'];
 	const generate = require('./scripts/minify-synthetix-contract');
@@ -27,43 +31,32 @@ function optimiseContracts(config, { webpack }) {
 	);
 }
 
-function generateBundleReport(config) {
-	if (process.env.GENERATE_BUNDLE_REPORT === 'true') {
-		const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
-		const plugin = new BundleAnalyzerPlugin({
-			analyzerMode: 'static',
-			reportFilename: 'reports/webpack.html',
-			openAnalyzer: false,
-			generateStatsFile: false,
-		});
-		config.plugins.push(plugin);
+module.exports = withPlugins(
+	[[optimizedImages, { images: { optimize: false } }], withBundleAnalyzer],
+	{
+		webpack: (config, context) => {
+			config.resolve.mainFields = ['module', 'browser', 'main'];
+			optimiseContracts(config, context);
+			return config;
+		},
+		trailingSlash: !!process.env.NEXT_PUBLIC_TRAILING_SLASH_ENABLED,
+		exportPathMap: function (defaultPathMap) {
+			return {
+				...defaultPathMap,
+
+				// all the dynamic pages need to be defined here (this needs to be imported from the routes)
+				'/staking': { page: '/staking/[[...action]]' },
+				'/staking/burn': { page: '/staking/[[...action]]' },
+				'/staking/mint': { page: '/staking/[[...action]]' },
+
+				'/earn': { page: '/earn/[[...pool]]' },
+				'/earn/claim': { page: '/earn/[[...pool]]' },
+				'/earn/curve-LP': { page: '/earn/[[...pool]]' },
+				'/earn/iBTC-LP': { page: '/earn/[[...pool]]' },
+				'/earn/iETH-LP': { page: '/earn/[[...pool]]' },
+
+				'/pools/weth-snx': { page: '/pools/[[...pool]]' },
+			};
+		},
 	}
-}
-
-module.exports = withPlugins([[optimizedImages, { images: { optimize: false } }]], {
-	webpack: (config, context) => {
-		config.resolve.mainFields = ['module', 'browser', 'main'];
-		generateBundleReport(config, context);
-		optimiseContracts(config, context);
-		return config;
-	},
-	trailingSlash: !!process.env.NEXT_PUBLIC_TRAILING_SLASH_ENABLED,
-	exportPathMap: function (defaultPathMap) {
-		return {
-			...defaultPathMap,
-
-			// all the dynamic pages need to be defined here (this needs to be imported from the routes)
-			'/staking': { page: '/staking/[[...action]]' },
-			'/staking/burn': { page: '/staking/[[...action]]' },
-			'/staking/mint': { page: '/staking/[[...action]]' },
-
-			'/earn': { page: '/earn/[[...pool]]' },
-			'/earn/claim': { page: '/earn/[[...pool]]' },
-			'/earn/curve-LP': { page: '/earn/[[...pool]]' },
-			'/earn/iBTC-LP': { page: '/earn/[[...pool]]' },
-			'/earn/iETH-LP': { page: '/earn/[[...pool]]' },
-
-			'/pools/weth-snx': { page: '/pools/[[...pool]]' },
-		};
-	},
-});
+);
