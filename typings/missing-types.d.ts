610baa1e200baed1d8becd3a4c371a9129b7b0ab
bachstatter <joey@cc.snxdao.io>
2022-07-14 17:16:59 +0200

    Add type declation for sockets missing types

--- typings/missing-types.d.ts
+++ typings/missing-types.d.ts
@@ -4,3 +4,5 @@ declare module '@ensdomains/ensjs';
 declare module '@eth-optimism/watcher';
 declare module 'remarkable/linkify';
 declare module 'remarkable-external-link';
+// todo remove when socket fixes their types
+declare module '@socket.tech/widget';
