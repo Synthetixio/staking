07ffef2d10fb6f2cf75cc8e9a428031182b0f63b
bachstatter <joey@cc.snxdao.io>
2022-07-27 18:15:46 +0100

    Refactor loans container. - Dont setup listenters for erc20Loan contracts - use react query to fetch the global state - remove all other logic added for erc20 contracts

--- containers/Loans/Loans.tsx
+++ containers/Loans/Loans.tsx
@@ -2,17 +2,15 @@ import { createContainer } from 'unstated-next';
 import { useMemo, useEffect, useState } from 'react';
 import { BigNumber, ethers } from 'ethers';
 
-import { renBTCToken } from 'contracts';
 import Connector from 'containers/Connector';
-
-import { LOAN_TYPE_ERC20, LOAN_TYPE_ETH } from 'sections/loans/constants';
 import { sleep } from 'utils/promise';
-import Wei, { wei } from '@synthetixio/wei';
 import getOpenLoans from './getOpenLoans';
 import { Loan } from './types';
 import getLoan from './getLoan';
-import getMinCRatios from './getMinCratios';
+
 import useSynthetixQueries from '@synthetixio/queries';
+import { useQuery } from 'react-query';
+import { wei } from '@synthetixio/wei';
 
 const SECONDS_IN_A_YR = 365 * 24 * 60 * 60;
 
@@ -22,51 +20,30 @@ function Container() {
 
 	const [isLoadingLoans, setIsLoadingLoans] = useState(false);
 	const [loans, setLoans] = useState<Loan[]>([]);
-	const [minCRatios, setMinCRatios] = useState<{
-		ethMinCratio: Wei | undefined;
-		erc20MinCratio: Wei | undefined;
-	}>({ ethMinCratio: undefined, erc20MinCratio: undefined });
+
 	const [pendingWithdrawals, setPendingWithdrawals] = useState(BigNumber.from('0'));
 	const { subgraph } = useSynthetixQueries();
-	const [
-		ethLoanContract,
-		erc20LoanContract,
-		ethLoanStateContract,
-		erc20LoanStateContract,
-		collateralManagerContract,
-	] = useMemo(() => {
-		if (!(isAppReady && synthetixjs && signer && walletAddress))
-			return [null, null, null, null, null];
+	const [ethLoanContract, ethLoanStateContract, collateralManagerContract] = useMemo(() => {
+		if (!(isAppReady && synthetixjs && signer && walletAddress)) return [null, null, null];
 		const {
 			contracts: {
 				CollateralEth: ethLoanContract,
-				CollateralErc20: erc20LoanContract,
 				CollateralStateEth: ethLoanStateContract,
-				CollateralStateErc20: erc20LoanStateContract,
 				CollateralManager: collateralManagerContract,
 			},
 		} = synthetixjs;
-		return [
-			ethLoanContract,
-			erc20LoanContract,
-			ethLoanStateContract,
-			erc20LoanStateContract,
-			collateralManagerContract,
-		];
+		return [ethLoanContract, ethLoanStateContract, collateralManagerContract];
 	}, [isAppReady, signer, synthetixjs, walletAddress]);
 
-	useEffect(() => {
-		if (!ethLoanContract) return;
-		let isMounted = true;
-		getMinCRatios({ ethLoanContract, erc20LoanContract }).then((minCRatios) => {
-			if (isMounted) {
-				setMinCRatios(minCRatios);
-			}
-		});
-		return () => {
-			isMounted = false;
-		};
-	}, [erc20LoanContract, ethLoanContract, isL2]);
+	const minCratioQuery = useQuery(
+		'minCratio',
+		async () => {
+			if (!ethLoanContract) return wei(0);
+			const minCratio = await ethLoanContract.minCratio();
+			return wei(minCratio);
+		},
+		{ enabled: Boolean(ethLoanContract) }
+	);
 
 	const subgraphOpenLoansQuery = subgraph.useGetLoans(
 		{ where: { isOpen: true, account: walletAddress } },
@@ -87,13 +64,7 @@ function Container() {
 		if (!(isAppReady && walletAddress && provider && ethLoanContract)) {
 			return;
 		}
-		if (!isL2 && !erc20LoanContract) return; // erc20LoanContract only exists on L1
 
-		const loanContracts = [erc20LoanContract, ethLoanContract];
-		const loanStateContracts: [ethers.Contract | null, ethers.Contract | null] = [
-			erc20LoanStateContract,
-			ethLoanStateContract,
-		];
 		let isMounted = true;
 		const unsubs: Array<Function> = [
 			() => {
@@ -103,9 +74,7 @@ function Container() {
 		const loadLoans = async () => {
 			setIsLoadingLoans(true);
 			const openLoans = await getOpenLoans({
-				erc20LoanContract,
 				ethLoanContract,
-				erc20LoanStateContract,
 				ethLoanStateContract,
 				address: walletAddress,
 				isL2,
@@ -119,118 +88,118 @@ function Container() {
 
 		// subscribe to loan open+close
 		const subscribe = () => {
-			loanContracts.forEach((loanContract, i) => {
-				if (!loanContract) return;
-				const loanStateContract = loanStateContracts[i];
-				const updateLoan = async (owner: string, id: BigNumber) => {
-					const loan = await getLoan({
-						id: id.toNumber(),
-						loanContract,
-						loanStateContract,
-						isL2,
-						address: walletAddress,
-					});
-					setLoans((originalLoans) => {
-						const loans = originalLoans.slice();
-						const idx = loans.findIndex((l) => l.id.eq(id));
-						if (~idx) {
-							loans[idx] = loan;
-						} else {
-							console.warn(`unknown loan(id=${id.toString()}, owner=${owner.toString()})`);
+			const loanContract = ethLoanContract;
+			const loanStateContract = ethLoanStateContract;
+			if (!loanContract) return;
+
+			const updateLoan = async (owner: string, id: BigNumber) => {
+				const loan = await getLoan({
+					id: id.toNumber(),
+					loanContract,
+					loanStateContract: ethLoanStateContract || null,
+					isL2,
+					address: walletAddress,
+				});
+				setLoans((originalLoans) => {
+					const loans = originalLoans.slice();
+					const idx = loans.findIndex((l) => l.id.eq(id));
+					if (~idx) {
+						loans[idx] = loan;
+					} else {
+						console.warn(`unknown loan(id=${id.toString()}, owner=${owner.toString()})`);
+					}
+					return loans;
+				});
+			};
+
+			const onLoanCreated = async (_address: string, id: BigNumber) => {
+				const loan = await getLoan({
+					id: id.toNumber(),
+					loanContract,
+					loanStateContract,
+					isL2,
+					address: walletAddress,
+				});
+				setLoans((loans) => [loan, ...loans]);
+			};
+
+			const onLoanClosed = (_owner: string, id: BigNumber) => {
+				setLoans((loans) => loans.filter((loan) => !loan.id.eq(id)));
+			};
+
+			const onCollateralDeposited = async (owner: string, id: BigNumber, amount: BigNumber) => {
+				setLoans((loans) =>
+					loans.map((loan) => {
+						if (loan.id.eq(id)) {
+							return { ...loan, collateral: loan.collateral.add(amount) };
 						}
-						return loans;
-					});
-				};
+						return loan;
+					})
+				);
+				await updateLoan(owner, id);
+			};
 
-				const onLoanCreated = async (_address: string, id: BigNumber) => {
-					const loan = await getLoan({
-						id: id.toNumber(),
-						loanContract,
-						loanStateContract,
-						isL2,
-						address: walletAddress,
-					});
-					setLoans((loans) => [loan, ...loans]);
-				};
+			const onCollateralWithdrawn = async (owner: string, id: BigNumber, amount: BigNumber) => {
+				setLoans((loans) =>
+					loans.map((loan) => {
+						if (loan.id.eq(id)) {
+							loan.collateral = loan.collateral.sub(amount);
+						}
+						return loan;
+					})
+				);
+				await updateLoan(owner, id);
+			};
 
-				const onLoanClosed = (_owner: string, id: BigNumber) => {
-					setLoans((loans) => loans.filter((loan) => !loan.id.eq(id)));
-				};
+			const onLoanRepaymentMade = async (
+				borrower: string,
+				_repayer: string,
+				id: BigNumber,
+				payment: BigNumber
+			) => {
+				setLoans((loans) =>
+					loans.map((loan) => {
+						if (loan.id.eq(id)) {
+							return { ...loan, amount: loan.amount.sub(payment) };
+						}
+						return loan;
+					})
+				);
+				await updateLoan(borrower, id);
+			};
 
-				const onCollateralDeposited = async (owner: string, id: BigNumber, amount: BigNumber) => {
-					setLoans((loans) =>
-						loans.map((loan) => {
-							if (loan.id.eq(id)) {
-								return { ...loan, collateral: loan.collateral.add(amount) };
-							}
-							return loan;
-						})
-					);
-					await updateLoan(owner, id);
-				};
+			const onLoanDrawnDown = async (owner: string, id: BigNumber, amount: BigNumber) => {
+				setLoans((loans) =>
+					loans.map((loan) => {
+						if (loan.id.eq(id)) {
+							return { ...loan, amount: loan.amount.add(amount) };
+						}
+						return loan;
+					})
+				);
+				await updateLoan(owner, id);
+			};
 
-				const onCollateralWithdrawn = async (owner: string, id: BigNumber, amount: BigNumber) => {
-					setLoans((loans) =>
-						loans.map((loan) => {
-							if (loan.id.eq(id)) {
-								loan.collateral = loan.collateral.sub(amount);
-							}
-							return loan;
-						})
-					);
-					await updateLoan(owner, id);
-				};
+			const loanCreatedEvent = loanContract.filters.LoanCreated(walletAddress);
+			const loanClosedEvent = loanContract.filters.LoanClosed(walletAddress);
+			const collateralDepositedEvent = loanContract.filters.CollateralDeposited(walletAddress);
+			const collateralWithdrawnEvent = loanContract.filters.CollateralWithdrawn(walletAddress);
+			const loanDrawnDownEvent = loanContract.filters.LoanDrawnDown(walletAddress);
+			const loanRepaymentMadeEvent = loanContract.filters.LoanRepaymentMade(walletAddress);
 
-				const onLoanRepaymentMade = async (
-					borrower: string,
-					_repayer: string,
-					id: BigNumber,
-					payment: BigNumber
-				) => {
-					setLoans((loans) =>
-						loans.map((loan) => {
-							if (loan.id.eq(id)) {
-								return { ...loan, amount: loan.amount.sub(payment) };
-							}
-							return loan;
-						})
-					);
-					await updateLoan(borrower, id);
-				};
+			loanContract.on(loanCreatedEvent, onLoanCreated);
+			loanContract.on(loanClosedEvent, onLoanClosed);
+			loanContract.on(collateralDepositedEvent, onCollateralDeposited);
+			loanContract.on(collateralWithdrawnEvent, onCollateralWithdrawn);
+			loanContract.on(loanDrawnDownEvent, onLoanDrawnDown);
+			loanContract.on(loanRepaymentMadeEvent, onLoanRepaymentMade);
 
-				const onLoanDrawnDown = async (owner: string, id: BigNumber, amount: BigNumber) => {
-					setLoans((loans) =>
-						loans.map((loan) => {
-							if (loan.id.eq(id)) {
-								return { ...loan, amount: loan.amount.add(amount) };
-							}
-							return loan;
-						})
-					);
-					await updateLoan(owner, id);
-				};
-
-				const loanCreatedEvent = loanContract.filters.LoanCreated(walletAddress);
-				const loanClosedEvent = loanContract.filters.LoanClosed(walletAddress);
-				const collateralDepositedEvent = loanContract.filters.CollateralDeposited(walletAddress);
-				const collateralWithdrawnEvent = loanContract.filters.CollateralWithdrawn(walletAddress);
-				const loanDrawnDownEvent = loanContract.filters.LoanDrawnDown(walletAddress);
-				const loanRepaymentMadeEvent = loanContract.filters.LoanRepaymentMade(walletAddress);
-
-				loanContract.on(loanCreatedEvent, onLoanCreated);
-				loanContract.on(loanClosedEvent, onLoanClosed);
-				loanContract.on(collateralDepositedEvent, onCollateralDeposited);
-				loanContract.on(collateralWithdrawnEvent, onCollateralWithdrawn);
-				loanContract.on(loanDrawnDownEvent, onLoanDrawnDown);
-				loanContract.on(loanRepaymentMadeEvent, onLoanRepaymentMade);
-
-				unsubs.push(() => loanContract.off(loanCreatedEvent, onLoanCreated));
-				unsubs.push(() => loanContract.off(loanClosedEvent, onLoanClosed));
-				unsubs.push(() => loanContract.off(collateralDepositedEvent, onCollateralDeposited));
-				unsubs.push(() => loanContract.off(collateralWithdrawnEvent, onCollateralWithdrawn));
-				unsubs.push(() => loanContract.off(loanDrawnDownEvent, onLoanDrawnDown));
-				unsubs.push(() => loanContract.off(loanRepaymentMadeEvent, onLoanRepaymentMade));
-			});
+			unsubs.push(() => loanContract.off(loanCreatedEvent, onLoanCreated));
+			unsubs.push(() => loanContract.off(loanClosedEvent, onLoanClosed));
+			unsubs.push(() => loanContract.off(collateralDepositedEvent, onCollateralDeposited));
+			unsubs.push(() => loanContract.off(collateralWithdrawnEvent, onCollateralWithdrawn));
+			unsubs.push(() => loanContract.off(loanDrawnDownEvent, onLoanDrawnDown));
+			unsubs.push(() => loanContract.off(loanRepaymentMadeEvent, onLoanRepaymentMade));
 		};
 
 		loadLoans();
@@ -244,81 +213,43 @@ function Container() {
 		walletAddress,
 		provider,
 		ethLoanContract,
-		erc20LoanContract,
 		ethLoanStateContract,
-		erc20LoanStateContract,
 		isL2,
 		network,
 		subgraphOpenLoansKey,
 	]);
 
-	const [interestRate, setInterestRate] = useState(wei(0));
-	const [issueFeeRates, setIssueFeeRates] = useState<Record<string, Wei>>({
-		[LOAN_TYPE_ERC20]: wei(0),
-		[LOAN_TYPE_ETH]: wei(0),
-	});
-	const [interactionDelays, setInteractionDelays] = useState<Record<string, Wei>>({
-		[LOAN_TYPE_ERC20]: wei(0),
-		[LOAN_TYPE_ETH]: wei(0),
-	});
-
-	const renBTCContract = useMemo(
-		(): ethers.Contract | null =>
-			!(network && signer) ? null : renBTCToken.makeContract(network['name'], signer),
-		[signer, network]
-	);
-
-	useEffect(() => {
-		if (!(isAppReady && collateralManagerContract && ethLoanContract)) {
-			return;
-		}
-
-		let isMounted = true;
-		const load = async () => {
-			const [
-				[borrowRate],
-				erc20BorrowIssueFeeRate,
-				ethBorrowIssueFeeRate,
-				erc20InteractionDelay,
-				ethInteractionDelay,
-			] = await Promise.all([
+	const rateAndDelayQueries = useQuery(
+		['rateAndDelayQuery', isL2],
+		async () => {
+			if (!collateralManagerContract || !ethLoanContract) {
+				throw Error('Expected contracts to be defined');
+			}
+			const [[borrowRate], ethBorrowIssueFeeRate, ethInteractionDelay] = await Promise.all([
 				collateralManagerContract.getBorrowRate(),
-				// erc20LoanContract doesn't exists in L2
-				erc20LoanContract?.issueFeeRate ? erc20LoanContract.issueFeeRate() : wei(0),
 				ethLoanContract.issueFeeRate(),
-				// Interaction delay doesn't exists in L2
-				erc20LoanContract?.interactionDelay ? erc20LoanContract.interactionDelay() : wei(0),
 				ethLoanContract.interactionDelay ? ethLoanContract.interactionDelay() : wei(0),
 			]);
+			const perYr = SECONDS_IN_A_YR / 1e18;
 
-			if (isMounted) {
-				const perYr = SECONDS_IN_A_YR / 1e18;
-				setInterestRate(wei(borrowRate.toString()).mul(wei(perYr.toString())));
-				setIssueFeeRates({
-					[LOAN_TYPE_ERC20]: wei(erc20BorrowIssueFeeRate.toString()).mul(1 / 1e18),
-					[LOAN_TYPE_ETH]: wei(ethBorrowIssueFeeRate.toString()).mul(1 / 1e18),
-				});
-				setInteractionDelays({
-					[LOAN_TYPE_ERC20]: wei(erc20InteractionDelay.toString()),
-					[LOAN_TYPE_ETH]: wei(ethInteractionDelay.toString()),
-				});
-			}
-		};
-
-		load();
-		return () => {
-			isMounted = false;
-		};
-	}, [isAppReady, collateralManagerContract, erc20LoanContract, ethLoanContract]);
+			return {
+				borrowRate: wei(borrowRate.toString()).mul(wei(perYr.toString())),
+				issueFeeRate: wei(ethBorrowIssueFeeRate.toString()).mul(1 / 1e18),
+				interactionDelay: wei(ethInteractionDelay.toString()),
+			};
+		},
+		{ enabled: Boolean(isAppReady && collateralManagerContract && ethLoanContract) }
+	);
 
 	// pending withdrawals
 	const loadPendingWithdrawals = async (
-		ethLoanContract: typeof erc20LoanContract,
+		ethLoanContract: ethers.Contract | null,
 		isMounted: boolean,
 		setPendingWithdrawals: (pw: BigNumber) => void,
 		address: string
 	) => {
-		const pw = await ethLoanContract!.pendingWithdrawals(address);
+		if (!ethLoanContract) return;
+		const pw = await ethLoanContract.pendingWithdrawals(address);
 		if (isMounted) {
 			setPendingWithdrawals(pw);
 		}
@@ -345,15 +276,13 @@ function Container() {
 	return {
 		loans,
 		isLoadingLoans,
-		interestRate,
-		issueFeeRates,
-		interactionDelays,
-		minCRatios,
+		interestRate: rateAndDelayQueries.data?.borrowRate || wei(0),
+		issueFeeRate: rateAndDelayQueries.data?.issueFeeRate || wei(0),
+		interactionDelay: rateAndDelayQueries.data?.interactionDelay || wei(0),
+		minCRatio: minCratioQuery.data,
 		pendingWithdrawals,
 		reloadPendingWithdrawals,
 		ethLoanContract,
-		erc20LoanContract,
-		renBTCContract,
 	};
 }
 export default createContainer(Container);
