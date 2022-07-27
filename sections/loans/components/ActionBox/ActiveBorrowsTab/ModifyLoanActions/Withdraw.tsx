97df48d8a018c2e3f4291bc6366aa800c88c1210
bachstatter <joey@cc.snxdao.io>
2022-07-27 12:00:50 +0100

    Refactor withdraw to useSynthetixTxn

--- sections/loans/components/ActionBox/ActiveBorrowsTab/ModifyLoanActions/Withdraw.tsx
+++ sections/loans/components/ActionBox/ActiveBorrowsTab/ModifyLoanActions/Withdraw.tsx
@@ -1,8 +1,6 @@
-import { useState, useCallback } from 'react';
+import { useState } from 'react';
 import { ethers } from 'ethers';
 import { Loan } from 'containers/Loans/types';
-import TransactionNotifier from 'containers/TransactionNotifier';
-import { tx } from 'utils/transactions';
 import Loans from 'containers/Loans';
 import Wrapper from './Wrapper';
 import { useRouter } from 'next/router';
@@ -10,22 +8,23 @@ import ROUTES from 'constants/routes';
 import { getETHToken } from 'contracts/ethToken';
 import { getRenBTCToken } from 'contracts/renBTCToken';
 import { wei } from '@synthetixio/wei';
+import useSynthetixQueries, { GasPrice } from '@synthetixio/queries';
 
 type WithdrawProps = {
 	loanId: number;
 	loanTypeIsETH: boolean;
 	loan: Loan;
-	loanContract: ethers.Contract;
 };
 
-const Withdraw: React.FC<WithdrawProps> = ({ loan, loanId, loanTypeIsETH, loanContract }) => {
+const Withdraw: React.FC<WithdrawProps> = ({ loan, loanId, loanTypeIsETH }) => {
 	const router = useRouter();
-	const { monitorTransaction } = TransactionNotifier.useContainer();
+	const [gasPrice, setGasPrice] = useState<GasPrice | undefined>(undefined);
+	const { useSynthetixTxn } = useSynthetixQueries();
+
 	const { reloadPendingWithdrawals } = Loans.useContainer();
 
 	const [isWorking, setIsWorking] = useState<string>('');
 	const [withdrawalAmountString, setWithdrawalAmount] = useState<string | null>(null);
-	const [error, setError] = useState<string | null>(null);
 	const [txModalOpen, setTxModalOpen] = useState<boolean>(false);
 
 	const collateralAsset = loanTypeIsETH ? 'ETH' : 'renBTC';
@@ -52,37 +51,39 @@ const Withdraw: React.FC<WithdrawProps> = ({ loan, loanId, loanTypeIsETH, loanCo
 	const onSetLeftColMaxAmount = () =>
 		setWithdrawalAmount(ethers.utils.formatUnits(collateralAmount.toBN(), collateralDecimals));
 
-	const getTxData = useCallback(() => {
-		if (!(loanContract && !withdrawalAmount.eq(0))) return null;
-		return [loanContract, 'withdraw', [loanId, withdrawalAmount.toBN()]];
-	}, [loanContract, loanId, withdrawalAmount]);
+	const contractName = loanTypeIsETH ? 'CollateralEth' : 'CollateralErc20';
 
-	const withdraw = async () => {
-		try {
-			setIsWorking('withdrawing');
-			setTxModalOpen(true);
-			await tx(() => getTxData(), {
-				showErrorNotification: (e: string) => setError(e),
-				showProgressNotification: (hash: string) =>
-					monitorTransaction({
-						txHash: hash,
-						onTxConfirmed: () => {},
-					}),
-			});
-			await reloadPendingWithdrawals();
-			setIsWorking('');
-			setTxModalOpen(false);
-			router.push(ROUTES.Loans.List);
-		} catch {
-			setIsWorking('');
-			setTxModalOpen(false);
+	const txn = useSynthetixTxn(
+		contractName,
+		'withdraw',
+		[loanId, withdrawalAmount.toBN()],
+		gasPrice,
+		{
+			enabled: !withdrawalAmount.eq(0),
+			onSuccess: async () => {
+				await reloadPendingWithdrawals();
+				setIsWorking('');
+				setTxModalOpen(false);
+				router.push(ROUTES.Loans.List);
+			},
+			onError: () => {
+				setIsWorking('');
+				setTxModalOpen(false);
+			},
 		}
+	);
+	const withdraw = async () => {
+		setIsWorking('withdrawing');
+		setTxModalOpen(true);
+		txn.mutate();
 	};
 
 	return (
 		<Wrapper
 			{...{
-				getTxData,
+				onGasPriceChange: setGasPrice,
+				gasLimit: txn.gasLimit,
+				optimismLayerOneFee: txn.optimismLayerOneFee,
 
 				loan,
 				loanTypeIsETH,
@@ -104,8 +105,7 @@ const Withdraw: React.FC<WithdrawProps> = ({ loan, loanId, loanTypeIsETH, loanCo
 				buttonIsDisabled: !!isWorking,
 				onButtonClick: withdraw,
 
-				error,
-				setError,
+				error: txn.errorMessage,
 
 				txModalOpen,
 				setTxModalOpen,
