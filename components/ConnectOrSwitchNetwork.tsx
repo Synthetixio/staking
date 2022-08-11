c35e3d64fb7d0fec9c702455e4715bb65c886b6b
bachstatter <joey@cc.snxdao.io>
2022-08-11 15:49:57 +0200

    Some more styling for ConnectOrSwitchNetwork

--- components/ConnectOrSwitchNetwork.tsx
+++ components/ConnectOrSwitchNetwork.tsx
@@ -13,7 +13,7 @@ const ConnectOrSwitchNetwork: React.FC = () => {
 	if (walletConnectedToUnsupportedNetwork) {
 		return (
 			<>
-				<p>{t('common.wallet.switch-to-supported')}</p>
+				<P>{t('common.wallet.switch-to-supported')}</P>
 				<ButtonContainer>
 					<StyledCTA
 						variant="primary"
@@ -39,24 +39,34 @@ const ConnectOrSwitchNetwork: React.FC = () => {
 	}
 	if (!isWalletConnected) {
 		return (
-			<StyledCTA variant="primary" size="lg" onClick={connectWallet}>
-				{t('common.wallet.connect-wallet')}
-			</StyledCTA>
+			<ButtonContainer>
+				<StyledCTA variant="primary" size="lg" onClick={connectWallet}>
+					{t('common.wallet.connect-wallet')}
+				</StyledCTA>
+			</ButtonContainer>
 		);
 	}
 	return null;
 };
 
+const P = styled.p`
+	text-align: center;
+	color: white;
+	text-transform: uppercase;
+	font-family: ${(props) => props.theme.fonts.condensedMedium};
+	font-size: 14px;
+`;
 const ButtonContainer = styled.div`
 	display: flex;
+	justify-content: center;
 `;
-
 const StyledCTA = styled(Button)`
 	font-size: 14px;
 	font-family: ${(props) => props.theme.fonts.condensedMedium};
 	box-shadow: 0px 0px 10px rgba(0, 209, 255, 0.9);
 	border-radius: 4px;
 	width: 100%;
+	max-width: 200px;
 	text-transform: uppercase;
 	margin: 0 10px;
 
