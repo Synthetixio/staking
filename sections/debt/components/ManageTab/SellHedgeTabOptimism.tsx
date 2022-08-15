7a055a1bc327e53ebc517520c743f1664ec30c3b
fritzschoff <fritzschoff@icloud.com>
2022-08-15 14:20:25 +0200

    finished interface for selling dSNX to sUSD

--- sections/debt/components/ManageTab/SellHedgeTabOptimism.tsx
+++ sections/debt/components/ManageTab/SellHedgeTabOptimism.tsx
@@ -1,44 +1,73 @@
 import useSynthetixQueries, { GasPrice } from '@synthetixio/queries';
 import { wei } from '@synthetixio/wei';
-import { dSNXPoolContractOptimism } from 'constants/dhedge';
 import Connector from 'containers/Connector';
 import { constants, utils } from 'ethers';
 import useGetDSnxBalance from 'hooks/useGetDSnxBalance';
 import useGetNeedsApproval from 'hooks/useGetNeedsApproval';
 import { useState } from 'react';
 import styled from 'styled-components';
-import { FlexDivColCentered } from 'styles/common';
+import {
+	FlexDivColCentered,
+	ModalContent,
+	ModalItem,
+	ModalItemText,
+	ModalItemTitle,
+} from 'styles/common';
 import { formatCryptoCurrency } from 'utils/formatters/number';
 import Dhedge from 'assets/svg/app/dhedge.svg';
+import LoaderIcon from 'assets/svg/app/loader.svg';
+import WarningIcon from 'assets/svg/app/warning.svg';
 import {
 	StyledBackgroundTab,
+	StyledBalance,
+	StyledButton,
 	StyledCryptoCurrencyBox,
 	StyledCryptoCurrencyImage,
 	StyledHedgeInput,
 	StyledInputLabel,
+	StyledMaxButton,
+	StyledSpacer,
 } from './hedge-tab-ui-components';
+import { dSNXExitSUSDContractOptimism, dSNXPoolContractOptimism } from 'constants/dhedge';
+import useGetDSNXPrice from 'hooks/useGetDSNXPrice';
+import GasSelector from 'components/GasSelector';
+import TxConfirmationModal from 'sections/shared/modals/TxConfirmationModal';
 
 export default function SellHedgeTabOptimism() {
-	const { useContractTxn } = useSynthetixQueries();
+	const { useContractTxn, useSynthsBalancesQuery } = useSynthetixQueries();
 	const [approveGasCost, setApproveGasCost] = useState<GasPrice | undefined>(undefined);
 	const [withdrawnGasCost, setWithdrawGasCost] = useState<GasPrice | undefined>(undefined);
 	const [amountToSend, setAmountToSend] = useState('');
 	const [txModalOpen, setTxModalOpen] = useState<boolean>(false);
 	const [sendMax, setSendMax] = useState<boolean>(false);
-	const { walletAddress } = Connector.useContainer();
+	const { walletAddress, signer } = Connector.useContainer();
 	const approveQuery = useGetNeedsApproval(
-		dSNXPoolContractOptimism.address,
+		dSNXExitSUSDContractOptimism.address,
 		dSNXPoolContractOptimism,
 		walletAddress
 	);
+	const dSNXTokenPriceQuery = useGetDSNXPrice(walletAddress);
+	const dSNXPrice = dSNXTokenPriceQuery.data;
+	const synthsBalancesQuery = useSynthsBalancesQuery(walletAddress);
+	const sUSDBalance = synthsBalancesQuery.data?.balancesMap.sUSD?.balance || wei(0);
 	const dSNXBalanceQuery = useGetDSnxBalance();
+	const actualAmountToSendBn = sendMax
+		? dSNXBalanceQuery.data?.toBN() || wei(0).toBN()
+		: wei(amountToSend || 0).toBN();
+	const dSnxAmount =
+		actualAmountToSendBn.gt(0) && dSNXPrice
+			? formatCryptoCurrency(wei(actualAmountToSendBn).mul(dSNXPrice), {
+					maxDecimals: 1,
+					minDecimals: 2,
+			  })
+			: '';
 	const approveTx = useContractTxn(
-		dSNXPoolContractOptimism,
+		dSNXPoolContractOptimism.connect(signer!),
 		'approve',
-		[dSNXPoolContractOptimism.address, constants.MaxUint256],
+		[dSNXExitSUSDContractOptimism.address, constants.MaxUint256],
 		approveGasCost,
 		{
-			enabled: Boolean(walletAddress && dSNXPoolContractOptimism),
+			enabled: Boolean(walletAddress && dSNXExitSUSDContractOptimism),
 			onSuccess: () => {
 				setTxModalOpen(false);
 				approveQuery.refetch();
@@ -46,9 +75,14 @@ export default function SellHedgeTabOptimism() {
 		}
 	);
 	const withdrawTx = useContractTxn(
-		dSNXPoolContractOptimism,
+		dSNXExitSUSDContractOptimism.connect(signer!),
 		'withdrawSUSD',
-		[dSNXPoolContractOptimism.address],
+		[
+			dSNXExitSUSDContractOptimism.address,
+			actualAmountToSendBn,
+			'0x7F5c764cBc14f9669B88837ca1490cCa17c31607',
+			wei(actualAmountToSendBn).mul(dSNXPrice).toBN(),
+		],
 		withdrawnGasCost,
 		{
 			onSuccess: () => {
@@ -90,6 +124,26 @@ export default function SellHedgeTabOptimism() {
 					value={sendMax ? dSNXBalanceQuery.data?.toString(2) || '0' : amountToSend}
 					autoFocus={true}
 				/>
+				<StyledBalance>
+					Balance:&nbsp;
+					{formatCryptoCurrency(wei(dSNXBalanceQuery.data), {
+						maxDecimals: 1,
+						minDecimals: 2,
+					})}
+					<StyledMaxButton
+						variant="text"
+						isActive={dSNXBalanceQuery.data?.gt(0)}
+						disabled={approveTx.isLoading || withdrawTx.isLoading}
+						onClick={() => {
+							if (sUSDBalance?.gt(0)) {
+								setSendMax(true);
+							}
+						}}
+					>
+						MAX
+					</StyledMaxButton>
+				</StyledBalance>
+				<StyledSpacer />
 				<StyledInputLabel>
 					Buying
 					<StyledCryptoCurrencyBox>
@@ -97,11 +151,98 @@ export default function SellHedgeTabOptimism() {
 						sUSD
 					</StyledCryptoCurrencyBox>
 				</StyledInputLabel>
+				<StyledHedgeInput
+					type="text"
+					onChange={() => {}}
+					disabled
+					value={dSnxAmount ? `~${dSnxAmount}` : ''}
+				/>
+				<StyledBalance>
+					Balance:&nbsp;
+					{formatCryptoCurrency(sUSDBalance, {
+						maxDecimals: 1,
+						minDecimals: 2,
+					})}
+				</StyledBalance>
+				<GasSelector
+					gasLimitEstimate={approveQuery.data ? withdrawTx.gasLimit : approveTx.gasLimit}
+					onGasPriceChange={approveQuery.data ? setWithdrawGasCost : setApproveGasCost}
+					optimismLayerOneFee={
+						approveQuery.data ? withdrawTx.optimismLayerOneFee : approveTx.optimismLayerOneFee
+					}
+					altVersion
+				/>
 			</StyledBackgroundTab>
+			{approveTx.isLoading && (
+				<LoaderContainer>
+					<LoaderIcon width="20" />
+					<LoaderText>Approving dSNX</LoaderText>
+				</LoaderContainer>
+			)}
+			{withdrawTx.isLoading && (
+				<LoaderContainer>
+					<LoaderIcon width="20" />
+					<LoaderText>Selling dSNX for sUSD</LoaderText>
+				</LoaderContainer>
+			)}
+			{Boolean(!approveTx.isLoading && !withdrawTx.isLoading) && (
+				<>
+					{withdrawTx.errorMessage && (
+						<ErrorText>
+							<WarningIcon width="30" />
+							{withdrawTx.errorMessage}
+						</ErrorText>
+					)}
+					<StyledButton
+						size="lg"
+						onClick={() => {
+							setTxModalOpen(true);
+							approveQuery.data ? withdrawTx.mutate() : approveTx.mutate();
+						}}
+						variant="primary"
+						disabled={wei(actualAmountToSendBn || '0').eq(0) || Boolean(withdrawTx.errorMessage)}
+					>
+						{approveQuery.data ? 'Swap' : 'Approve'}
+					</StyledButton>
+				</>
+			)}
+			{txModalOpen && (
+				<TxConfirmationModal
+					onDismiss={() => setTxModalOpen(false)}
+					txError={approveQuery.data ? withdrawTx.errorMessage : approveTx.errorMessage}
+					attemptRetry={approveQuery.data ? withdrawTx.mutate : approveTx.mutate}
+					content={
+						<ModalContent>
+							<ModalItem>
+								<ModalItemTitle>
+									{approveQuery.data ? 'Selling dSNX for sUSD' : 'Approving dSNX'}
+								</ModalItemTitle>
+								<ModalItemText>{dSNXPoolContractOptimism.address}</ModalItemText>
+							</ModalItem>
+						</ModalContent>
+					}
+				/>
+			)}
 		</StyledSellHedgeContainer>
 	);
 }
 const StyledSellHedgeContainer = styled(FlexDivColCentered)`
 	width: 100%;
 	min-height: 100%;
+`;
+const ErrorText = styled.p`
+	color: white;
+	text-transform: none;
+	font-size: 14px;
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+`;
+const LoaderContainer = styled.div`
+	display: flex;
+	align-items: center;
+`;
+const LoaderText = styled.p`
+	margin-left: 10px;
+	text-transform: none;
 `;
