5134f97b265b3a29d35d96d5f395b5a3508f6e6c
fritzschoff <fritzschoff@icloud.com>
2022-08-22 16:37:09 +0200

    feat(alert): added fade in animation

--- components/Alert/index.tsx
+++ components/Alert/index.tsx
@@ -1,5 +1,5 @@
 import { InfoIcon } from '@chakra-ui/icons';
-import { Container, Text } from '@chakra-ui/react';
+import { Container, Fade, Text } from '@chakra-ui/react';
 
 interface AlterProps {
   text: string;
@@ -7,21 +7,23 @@ interface AlterProps {
 
 export default function Alert({ text }: AlterProps) {
   return (
-    <Container
-      borderLeftWidth={5}
-      borderLeftStyle="solid"
-      borderLeftColor="cyan.500"
-      borderRadius={5}
-      display="flex"
-      alignItems="center"
-      padding={1}
-      paddingLeft={3}
-      bg="blue.900"
-    >
-      <InfoIcon w={4} h={4} color="cyan.500" marginRight={3} />
-      <Text fontSize="sm" fontFamily="body">
-        {text}
-      </Text>
-    </Container>
+    <Fade in={true}>
+      <Container
+        borderLeftWidth={5}
+        borderLeftStyle="solid"
+        borderLeftColor="cyan.500"
+        borderRadius={5}
+        display="flex"
+        alignItems="center"
+        padding={1}
+        paddingLeft={3}
+        bg="blue.900"
+      >
+        <InfoIcon w={4} h={4} color="cyan.500" marginRight={3} />
+        <Text fontSize="sm" fontFamily="body">
+          {text}
+        </Text>
+      </Container>
+    </Fade>
   );
 }
