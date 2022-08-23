f6cb1e4c02a55b44aab25d88274f89b7b7114067
bachstatter <joey@cc.snxdao.io>
2022-08-23 11:38:44 +1000

    Add useStakedSNX hook using staking-ratio api

---
+++ hooks/useStakedSNX.ts
@@ -0,0 +1,24 @@
+import { useQuery } from 'react-query';
+
+const STAKED_SNX_DATA_URL = 'https://api.synthetix.io/staking-ratio';
+
+type StakedSNXResponse = {
+  cratio: number;
+  timestamp: number;
+  stakedSnx: {
+    ethereum: number;
+    optimism: number;
+  };
+};
+export const useStakedSNX = () => {
+  return useQuery(
+    'debt-pool-data',
+    async () => {
+      const resp = await fetch(STAKED_SNX_DATA_URL);
+
+      const data: StakedSNXResponse = await resp.json();
+      return data;
+    },
+    { staleTime: 10000 }
+  );
+};
