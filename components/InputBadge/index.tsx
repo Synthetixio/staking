345dc51aa85252070a0a404d532051c312e2f422
fritzschoff <fritzschoff@icloud.com>
2022-08-23 08:35:31 +0200

    feat(input-badge): removed styled components

--- components/InputBadge/index.tsx
+++ components/InputBadge/index.tsx
@@ -1,14 +1,13 @@
 import { InfoIcon } from '@chakra-ui/icons';
-import { Badge, BadgeProps, Flex, Text } from '@chakra-ui/react';
+import { Badge, BadgeProps, Fade, Flex, Text } from '@chakra-ui/react';
 import { PropsWithChildren } from 'react';
-import styled from 'styled-components';
-import { motion } from 'framer-motion';
 
 interface InputBadgeProps {
   colorScheme?: BadgeProps['colorScheme'];
   text: string;
   onClickIcon?: () => void;
   onClick?: () => void;
+  active: boolean;
 }
 
 export default function InputBadge({
@@ -16,23 +15,33 @@ export default function InputBadge({
   text,
   onClickIcon,
   onClick,
+  active,
 }: PropsWithChildren<InputBadgeProps>) {
   return (
-    <StyledAnimatedWrapper animate={{ x: 100 }} transition={{ ease: 'easeOut', duration: 1 }}>
-      <Badge colorScheme={colorScheme} padding="1.5" onClick={onClick} minWidth={170}>
+    <Fade in>
+      <Badge
+        colorScheme={active ? 'cyan' : colorScheme}
+        padding="1.5"
+        variant={active ? 'solid' : 'subtle'}
+        onClick={onClick}
+        w="100%"
+        minW={170}
+        cursor="pointer"
+      >
         <Flex justify="space-between" align="center">
-          <Text fontSize="xs" color="cyan.500" fontFamily="body">
+          <Text
+            fontSize="xs"
+            color={active ? 'black' : 'cyan.500'}
+            fontFamily="body"
+            userSelect="none"
+          >
             {text}
           </Text>
-          {onClickIcon && <InfoIcon w={4} h={4} color="cyan.500" onClick={onClickIcon} />}
+          {onClickIcon && (
+            <InfoIcon w={4} h={4} color={active ? 'black' : 'cyan.500'} onClick={onClickIcon} />
+          )}
         </Flex>
       </Badge>
-    </StyledAnimatedWrapper>
+    </Fade>
   );
 }
-
-const StyledAnimatedWrapper = styled(motion.div)`
-  min-width: 170px;
-  border: 1px solid black;
-  cursor: pointer;
-`;
--- content/BurnPage.tsx
+++ content/BurnPage.tsx
@@ -1,15 +1,23 @@
 import InputBadge from 'components/InputBadge';
 import Head from 'next/head';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 export default function BurnDebt() {
   const { t } = useTranslation();
+  const [active, setActive] = useState(false);
   return (
     <>
       <Head>
         <title>{t('burn.title')}</title>
       </Head>
-      <InputBadge colorScheme="whiteAlpha" text="test" onClickIcon={() => {}} />
+      <InputBadge
+        colorScheme="whiteAlpha"
+        text="test"
+        onClick={() => setActive(!active)}
+        onClickIcon={() => {}}
+        active={active}
+      />
     </>
   );
 }
