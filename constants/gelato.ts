3d62544c2cd3c99229630fd9cc0ea40a7fe8c6a7
Nikita <nikita@cc.snxdao.io>
2022-08-24 15:51:14 +0800

    Switch to use correct library imports (officially exported values)

--- constants/gelato.ts
+++ constants/gelato.ts
@@ -1,13 +1,13 @@
 import { Contract } from 'ethers';
-import ERC20ABI from '@synthetixio/queries/build/node/src/abis/ERC20.json';
+import { ERC20Abi } from '@synthetixio/queries';
 
 export const WETHSNXLPTokenContract = new Contract(
   '0x83bEeFB4cA39af649D03969B442c0E9F4E1732D8',
-  ERC20ABI
+  ERC20Abi
 );
 export const SUSDDAILPTokenContract = new Contract(
   '0x88ccDBbA89E073C5DC08B9c84dfc1fDc152c0dAc',
-  ERC20ABI
+  ERC20Abi
 );
 
 export const gelatoGraphURL =
