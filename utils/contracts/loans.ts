c45ee3b05dac895a8883c2e6e76929e68fb6bab8
bachstatter <joey@cc.snxdao.io>
2022-08-25 10:39:42 +1000

    Add missing loans contracts

--- utils/contracts/loans.ts
+++ utils/contracts/loans.ts
@@ -19,6 +19,40 @@ import {
   address as CollateralEthAddressGoerliOvm,
   abi as CollateralEthAbiGoerliOvm,
 } from '@synthetixio/contracts/build/goerli-ovm/deployment/CollateralEth';
+// CollateralManager
+import {
+  name as CollateralManagerMainnet,
+  address as CollateralManagerAddressMainnet,
+  abi as CollateralManagerAbiMainnet,
+} from '@synthetixio/contracts/build/mainnet/deployment/CollateralManager';
+import {
+  name as CollateralManagerMainnetOvm,
+  address as CollateralManagerAddressMainnetOvm,
+  abi as CollateralManagerAbiMainnetOvm,
+} from '@synthetixio/contracts/build/mainnet-ovm/deployment/CollateralManager';
+import {
+  name as CollateralManagerGoerli,
+  address as CollateralManagerAddressGoerli,
+  abi as CollateralManagerAbiGoerli,
+} from '@synthetixio/contracts/build/goerli/deployment/CollateralManager';
+import {
+  name as CollateralManagerGoerliOvm,
+  address as CollateralManagerAddressGoerliOvm,
+  abi as CollateralManagerAbiGoerliOvm,
+} from '@synthetixio/contracts/build/goerli-ovm/deployment/CollateralEth';
+// CollateralStateEth
+import {
+  name as CollateralStateEthMainnet,
+  address as CollateralStateEthAddressMainnet,
+  abi as CollateralStateEthAbiMainnet,
+} from '@synthetixio/contracts/build/mainnet/deployment/CollateralStateEth';
+
+// Not deployed yet
+// import {
+//   name as CollateralStateEthGoerli,
+//   address as CollateralStateEthAddressGoerli,
+//   abi as CollateralStateEthAbiGoerli,
+// } from '@synthetixio/contracts/build/goerli/deployment/CollateralStateEth';
 
 export const contracts = {
   CollateralEth: {
@@ -42,5 +76,40 @@ export const contracts = {
       address: CollateralEthAddressGoerliOvm,
       abi: CollateralEthAbiGoerliOvm,
     },
+  },
+  CollateralManager: {
+    mainnet: {
+      name: CollateralManagerMainnet,
+      address: CollateralManagerAddressMainnet,
+      abi: CollateralManagerAbiMainnet,
+    },
+    'mainnet-ovm': {
+      name: CollateralManagerMainnetOvm,
+      address: CollateralManagerAddressMainnetOvm,
+      abi: CollateralManagerAbiMainnetOvm,
+    },
+    goerli: {
+      name: CollateralManagerGoerli,
+      address: CollateralManagerAddressGoerli,
+      abi: CollateralManagerAbiGoerli,
+    },
+    'goerli-ovm': {
+      name: CollateralManagerGoerliOvm,
+      address: CollateralManagerAddressGoerliOvm,
+      abi: CollateralManagerAbiGoerliOvm,
+    },
+  },
+  CollateralStateEth: {
+    mainnet: {
+      name: CollateralStateEthMainnet,
+      address: CollateralStateEthAddressMainnet,
+      abi: CollateralStateEthAbiMainnet,
+    },
+    // Not deployed yet
+    // goerli: {
+    //   name: CollateralStateEthGoerli,
+    //   address: CollateralStateEthAddressGoerli,
+    //   abi: CollateralStateEthAbiGoerli,
+    // },
   },
 };
