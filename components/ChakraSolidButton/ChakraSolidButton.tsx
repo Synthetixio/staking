3e9ab822bf47af7fa488edde196f8a0c650ae162
bachstatter <joey@cc.snxdao.io>
2022-08-26 16:59:51 +1000

    Trigger deploy

--- components/ChakraSolidButton/ChakraSolidButton.tsx
+++ components/ChakraSolidButton/ChakraSolidButton.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@chakra-ui/react';
 import { FC } from 'react';
-
+//
 const ChakraSolidButton: FC = ({ children }) => {
   return (
     <Button p={2} bg={'salmon'} variant="solid">
