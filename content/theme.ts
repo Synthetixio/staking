5a4a7c7a50f0e18d0a5f6175a3a9ec84fcb1b993
jmzwar <james.zwar@protonmail.com>
2022-08-29 15:54:16 +1000

    wip

--- content/theme.ts
+++ content/theme.ts
@@ -3,6 +3,29 @@ import { theme as chakraTheme } from '@synthetixio/v3-theme';
 
 export const stakingTheme: Partial<ChakraTheme> = {
   ...chakraTheme,
+  components: {
+    Button: {
+      baseStyle: {
+        fontWeight: 700,
+        borderRadius: 'base',
+        color: 'black',
+        fontFamily: 'heading',
+        lineHeight: '20px',
+      },
+      variants: {
+        connect: {
+          bgGradient: chakraTheme.gradients['green-cyan']['500'],
+          _hover: {
+            bgGradient: chakraTheme.gradients['green-cyan']['600'],
+          },
+          _active: {
+            bgGradient: chakraTheme.gradients['green-cyan']['700'],
+          },
+        },
+      },
+    },
+    Text: {},
+  },
   styles: {
     global: {
       body: {
